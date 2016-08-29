fec20633607bbb6e4150082881f128da1518e57e
Aethys256 <aethys.dev@gmail.com>
2016-08-29 03:13:21 +0200

    Forgot this, we don't want to go further with OnGCD
    Even if it's not needed there :D

--- APL/Rogue_Outlaw.lua
+++ APL/Rogue_Outlaw.lua
@@ -4,9 +4,11 @@ local function APL ()
 	-- Saber Slash
 	if UnitPower("player", SPELL_POWER_COMBO_POINTS) < 5 then
 		ER.CastGCD(193315);
+		return;
 	-- Run Through
 	else
 		ER.CastGCD(2098);
+		return;
 	end
 end
 
