35d65557f765e101346683f8ed11a070b5a822fc
Aethys256 <aethys256@users.noreply.github.com>
2017-01-17 15:30:34 +0100

    [Commons] Rogue Functions

---
+++ APL/Commons/Everyone.lua
@@ -0,0 +1,12 @@
+local addonName, ER = ...;
+
+--- Localize Vars
+-- ER
+local Unit = ER.Unit;
+local Player = Unit.Player;
+local Target = Unit.Target;
+local Spell = ER.Spell;
+local Item = ER.Item;
+-- Commons
+ER.Commons = {};
+ER.Commons.Everyone = {};
---
+++ APL/Commons/Warrior.lua
@@ -0,0 +1,11 @@
+local addonName, ER = ...;
+
+--- Localize Vars
+-- ER
+local Unit = ER.Unit;
+local Player = Unit.Player;
+local Target = Unit.Target;
+local Spell = ER.Spell;
+local Item = ER.Item;
+-- Commons
+ER.Commons.Warrior = {};
