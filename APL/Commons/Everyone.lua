13e9ef8631fb4d2eb1cfcfdf17ffd1a6ccb45918
Aethys256 <aethys256@users.noreply.github.com>
2017-01-19 20:02:20 +0100

    [Commons] Everyone Interrupt

--- APL/Commons/Everyone.lua
+++ APL/Commons/Everyone.lua
@@ -10,3 +10,23 @@ local Item = ER.Item;
 -- Commons
 ER.Commons = {};
 ER.Commons.Everyone = {};
+-- GUI Settings
+local Settings = ER.GUISettings.General;
+
+
+-- Interrupt
+function ER.Commons.Interrupt (Range, Spell, Setting, StunSpells)
+  if Settings.InterruptEnabled and Target:IsInterruptible() and Target:IsInRange(Range) then
+    if Spell:IsCastable() then
+      if ER.Cast(Spell, Setting) then return "Cast " .. Spell:Name() .. " (Interrupt)"; end
+    elseif Settings.InterruptWithStun and Target:CanBeStunned() then
+      if StunSpells then
+        for i = 1, #StunSpells do
+          if StunSpells[i][1]:IsCastable() and StunSpells[i][3]() then
+            if ER.Cast(StunSpells[i][1]) then return StunSpells[i][2]; end
+          end
+        end
+      end
+    end
+  end
+end
