6f1c3a721553bfd6a6b703350bd28095a17f160c
Tae-l <thibaut.rahm.tr@gmail.com>
2017-01-21 19:12:08 +0100

    [APL] Enhancement
    Finished Enh APL, and added Enh to Settings asOffGCD

--- Settings.lua
+++ Settings.lua
@@ -159,5 +159,26 @@ ER.GUISettings = {
         }
       }
     }
+    Shaman = {
+      Enhancement = {
+          --{Display GCD as OffGcd, ForceReturn}
+          GCDasOffGCD = {
+          --Abilities
+          FeralSpirit = {true, false}
+          },
+          --{Display OffGCD as OffGCD, ForceReturn}
+          OffGCDasOffGCD = {
+          -- Abilities 
+          DoomWinds = {true, false},
+          --Legendaries
+          StormTempests = {true, false},
+          EmalonChargedCore = {true, false},
+          -- Kick
+          WindShear = {true, false},
+          -- Racial
+          Berserking = {true, false}
+        }
+    }
+  }
   }
 };
