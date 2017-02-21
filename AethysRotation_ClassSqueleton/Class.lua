97a0459c1a01d4b3b449ceae60146c5106e71e5d
Aethys256 <aethys.dev@gmail.com>
2017-02-21 23:04:37 +0100

    [Example] Added ClassSqueleton

---
+++ AethysRotation_ClassSqueleton/Class.lua
@@ -0,0 +1,23 @@
+--- Localize Vars
+  -- Addon
+  local addonName, addonTable = ...;
+  -- AethysCore
+  local AC = AethysCore;
+  local Cache = AethysCore_Cache;
+  local Unit = AC.Unit;
+  local Player = Unit.Player;
+  local Target = Unit.Target;
+  local Spell = AC.Spell;
+  local Item = AC.Item;
+  -- AethysRotation
+  local AR = AethysRotation;
+  -- Lua
+  
+  -- Commons
+  AR.Commons.Class = {};
+  -- GUI Settings
+  local Settings = AR.GUISettings.APL.Class.Commons;
+
+
+--- Commons Functions
+  
