e0e09db7a288b16c8590a2fd499fabdd42e21766
Aethys256 <aethys256@users.noreply.github.com>
2017-04-08 06:37:37 +0200

    [Paladin] Move Settings

---
+++ AethysRotation_Paladin/Settings.lua
@@ -0,0 +1,29 @@
+--- ============================ HEADER ============================
+--- ======= LOCALIZE =======
+  -- Addon
+  local addonName, addonTable = ...;
+  -- AethysRotation
+  local AR = AethysRotation;
+
+--- ============================ CONTENT ============================
+  -- All settings here should be moved into the GUI someday.
+  AR.GUISettings.APL.Paladin = {
+    Retribution = {
+      -- SoloMode Settings
+      SoloJusticarDP = 80, -- % HP threshold to use Justicar's Vengeance with Divine Purpose proc.
+      SoloJusticar5HP = 60, -- % HP threshold to use Justicar's Vengeance with 5 Holy Power.
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+        HolyWrath = {true, false}
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Racials
+        ArcaneTorrent = {true, false},
+        -- Abilities
+        AvengingWrath = {true, false},
+        Crusade = {true, false}
+      }
+    }
+  };
