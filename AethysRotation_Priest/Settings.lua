6a7db5c5ced0e7f1073a1aed44ebee5f9d5c9f57
Aethys256 <aethys256@users.noreply.github.com>
2017-04-08 06:38:40 +0200

    [Priest] Move Settings

---
+++ AethysRotation_Priest/Settings.lua
@@ -0,0 +1,32 @@
+--- ============================ HEADER ============================
+--- ======= LOCALIZE =======
+  -- Addon
+  local addonName, addonTable = ...;
+  -- AethysRotation
+  local AR = AethysRotation;
+
+--- ============================ CONTENT ============================
+  -- All settings here should be moved into the GUI someday.
+  AR.GUISettings.APL.Priest = {
+    Commons = {
+    },
+    Shadow = {
+      DispersionHP = 10,
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+        Shadowfiend = {true, false},
+        Shadowform = {true, false}
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Abilities
+        PowerInfusion = {true, false},
+        Dispersion = {true, false},
+        -- Racials
+        ArcaneTorrent = {true, false},
+        Berserking = {true, false},
+        BloodFury = {true, false},
+      }
+    },
+  };
