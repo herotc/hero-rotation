0b507060ee051ddeef4e68009f004cf868f0e957
Aethys256 <aethys256@users.noreply.github.com>
2017-04-08 06:36:00 +0200

    [Hunter] Move Settings

---
+++ AethysRotation_Hunter/Settings.lua
@@ -0,0 +1,65 @@
+--- ============================ HEADER ============================
+--- ======= LOCALIZE =======
+  -- Addon
+  local addonName, addonTable = ...;
+  -- AethysRotation
+  local AR = AethysRotation;
+
+--- ============================ CONTENT ============================
+  -- All settings here should be moved into the GUI someday.
+  AR.GUISettings.APL.Hunter = {
+    Commons = {
+      -- SoloMode Settings
+      ExhilarationHP = 30,
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+        Exhilaration = {true, false}
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Racials
+        ArcaneTorrent = {true, false},
+        Berserking = {true, false},
+        BloodFury = {true, false},
+        -- Abilities
+        CounterShot = {true, false}
+      }
+    },
+    BeastMastery = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+        Volley = {true, false}
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Abilities
+        AspectoftheWild = {true, false},
+        BeastialWrath = {true, false},
+        TitansThunder = {true, false}
+      }
+    },
+    Marksmanship = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+        Volley = {true, false}
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+      }
+    },
+    Survival = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Abilities
+        AspectoftheEagle = {true, false},
+        Butchery = {true, false},
+        SnakeHunter = {true, false},
+      }
+    }
+  };
