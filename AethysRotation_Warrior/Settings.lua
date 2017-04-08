7819c28c5a5161a752eb864e7c8826b51f11053f
Aethys256 <aethys256@users.noreply.github.com>
2017-04-08 06:40:31 +0200

    [Warrior] Move Settings

---
+++ AethysRotation_Warrior/Settings.lua
@@ -0,0 +1,46 @@
+--- ============================ HEADER ============================
+--- ======= LOCALIZE =======
+  -- Addon
+  local addonName, addonTable = ...;
+  -- AethysRotation
+  local AR = AethysRotation;
+
+--- ============================ CONTENT ============================
+  -- All settings here should be moved into the GUI someday.
+  AR.GUISettings.APL.Warrior = {
+    Commons = {
+      -- SoloMode Settings
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Racials
+        ArcaneTorrent = {true, false},
+        Berserking = {true, false},
+        BloodFury = {true, false},
+        -- Abilities
+        Pummel = {true, false}
+      }
+    },
+    Arms = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+      }
+    },
+    Fury = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Abilities
+        Avatar = {true, false},
+        BattleCry = {true, false}
+      }
+    }
+  };
