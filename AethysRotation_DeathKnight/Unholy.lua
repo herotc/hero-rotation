4f6b285806749c304894e77b5204cfd0cd049abe
chrislopez24 <pagarizabal2@gmail.com>
2017-06-09 04:23:25 +0200

    typo

--- AethysRotation_DeathKnight/Unholy.lua
+++ AethysRotation_DeathKnight/Unholy.lua
@@ -138,7 +138,7 @@
   if AR.Cast(S.DeathCoil) then return ""; end
  end
   --actions.generic+=/death_coil,if=talent.dark_arbiter.enabled&buff.sudden_doom.up&cooldown.dark_arbiter.remains>5&rune<=3
- if S.DeathCoil:IsCastable() and S.DarkArbiter:IsAvailable() and Player:Buff(S.SuddenDoom) and S.DarkArbiter:Cooldown() > 5 and Player:Runes() <= 3 then
+ if S.DeathCoil:IsUsable() and S.DarkArbiter:IsAvailable() and Player:Buff(S.SuddenDoom) and S.DarkArbiter:Cooldown() > 5 and Player:Runes() <= 3 then
   if AR.Cast(S.DeathCoil) then return ""; end
  end
   --actions.generic+=/festering_strike,if=debuff.festering_wound.stack<6&cooldown.apocalypse.remains<=6
@@ -213,12 +213,12 @@ local function Instructors()
   if AR.Cast(S.FesteringStrike) then return ""; end
  end 
 --actions.instructors+=/death_coil,if=!buff.necrosis.up&talent.necrosis.enabled&rune<=3
-if S.DeathCoil:IsCastable() and not Player:Buff(S.NecrosisBuff) and S.Necrosis:IsAvailable() and Player:Runes() <= 3 then
+if S.DeathCoil:IsUsable() and not Player:Buff(S.NecrosisBuff) and S.Necrosis:IsAvailable() and Player:Runes() <= 3 then
  if AR.Cast(S.DeathCoil) then return ""; end --TODO: maybe add sudden_doom buff condition and RP >= 35
  end 
 --actions.instructors+=/scourge_strike,if=buff.necrosis.react&debuff.festering_wound.stack>=4&runic_power.deficit>29
  if S.ScourgeStrike:IsCastable() and Player:Buff(S.NecrosisBuff) and Target:DebuffStack(S.FesteringWounds) >= 4 and Player:RunicPowerDeficit() > 29 and Player:Runes() >= 1 then
-  if AR.Cast(ScourgeStrike) then return ""; end
+  if AR.Cast(S.ScourgeStrike) then return ""; end
  end 
 --actions.instructors+=/clawing_shadows,if=buff.necrosis.react&debuff.festering_wound.stack>=3&runic_power.deficit>11
  if S.ClawingShadows:IsCastable() and Player:Buff(S.NecrosisBuff) and Target:DebuffStack(S.FesteringWounds) >= 3 and Player:RunicPowerDeficit() > 11 and Player:Runes() >= 1 then
@@ -226,7 +226,7 @@ if S.DeathCoil:IsCastable() and not Player:Buff(S.NecrosisBuff) and S.Necrosis:I
  end
 -- actions.instructors+=/scourge_strike,if=buff.unholy_strength.react&debuff.festering_wound.stack>=4&runic_power.deficit>29
  if S.ScourgeStrike:IsCastable() and Player:Buff(S.UnholyStrength) and Target:DebuffStack(S.FesteringWounds) >= 4 and Player:RunicPowerDeficit() > 29 and Player:Runes() >= 1 then
-  if AR.Cast(ScourgeStrike) then return ""; end
+  if AR.Cast(S.ScourgeStrike) then return ""; end
  end
 --actions.instructors+=/clawing_shadows,if=buff.unholy_strength.react&debuff.festering_wound.stack>=3&runic_power.deficit>11
  if S.ClawingShadows:IsCastable() and Player:Buff(S.UnholyStrength) and Target:DebuffStack(S.FesteringWounds) >= 3 and Player:RunicPowerDeficit() > 11 and Player:Runes() >= 1 then
@@ -241,24 +241,24 @@ if S.DeathCoil:IsCastable() and not Player:Buff(S.NecrosisBuff) and S.Necrosis:I
   if AR.Cast(S.ClawingShadows) then return ""; end
  end
 --actions.instructors+=/death_coil,if=talent.shadow_infusion.enabled&talent.dark_arbiter.enabled&!buff.dark_transformation.up&cooldown.dark_arbiter.remains>15
- if S.DeathCoil:IsCastable() and S.ShadowInfusion:IsAvailable() and S.DarkArbiter:IsAvailable() and not Pet:Buff(S.DarkTransformation) and S.DarkArbiter:Cooldown() > 15 then
+ if S.DeathCoil:IsUsable() and S.ShadowInfusion:IsAvailable() and S.DarkArbiter:IsAvailable() and not Pet:Buff(S.DarkTransformation) and S.DarkArbiter:Cooldown() > 15 then
   if AR.Cast(S.DeathCoil) then return ""; end
  end
 --actions.instructors+=/death_coil,if=talent.shadow_infusion.enabled&!talent.dark_arbiter.enabled&!buff.dark_transformation.up
- if S.DeathCoil:IsCastable() and S.ShadowInfusion:IsAvailable() and not S.DarkArbiter:IsAvailable() and not Pet:Buff(S.DarkTransformation) then 
+ if S.DeathCoil:IsUsable() and S.ShadowInfusion:IsAvailable() and not S.DarkArbiter:IsAvailable() and not Pet:Buff(S.DarkTransformation) then 
   if AR.Cast(S.DeathCoil) then return ""; end
  end 
 --actions.instructors+=/death_coil,if=talent.dark_arbiter.enabled&cooldown.dark_arbiter.remains>15
- if S.DeathCoil:IsCastable() and S.DarkArbiter:IsAvailable() and  S.DarkArbiter:Cooldown() > 15 then 
+ if S.DeathCoil:IsUsable() and S.DarkArbiter:IsAvailable() and  S.DarkArbiter:Cooldown() > 15 then 
   if AR.Cast(S.DeathCoil) then return ""; end
  end 
 --actions.instructors+=/death_coil,if=!talent.shadow_infusion.enabled&!talent.dark_arbiter.enabled
- if S.DeathCoil:IsCastable() and not S.ShadowInfusion:IsAvailable() and not S.DarkArbiter:IsAvailable() then
+ if S.DeathCoil:IsUsable() and not S.ShadowInfusion:IsAvailable() and not S.DarkArbiter:IsAvailable() then
   if AR.Cast(S.DeathCoil) then return ""; end
  end
   
 -- just making sure sudden_doom procs are used with Instructors
- if S.DeathCoil:IsCastable() and Player:Buff(SuddenDoom) and not Player:Buff(S.NecrosisBuff) then
+ if S.DeathCoil:IsUsable() and Player:Buff(SuddenDoom) and not Player:Buff(S.NecrosisBuff) then
   if AR.Cast(S.DeathCoil) then return ""; end
  end
    return false;
