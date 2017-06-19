9bba3d9636b102355c0811bb74081a90fa4aaf3d
lithium720 <lithium720@users.noreply.github.com>
2017-06-18 19:40:04 -0700

    [Enhancement] Various changes and fixes

--- AethysRotation_Shaman/Settings.lua
+++ AethysRotation_Shaman/Settings.lua
@@ -1,30 +1,31 @@
 --- ============================ HEADER ============================
 --- ======= LOCALIZE =======
-  -- Addon
-  local addonName, addonTable = ...;
-  -- AethysRotation
-  local AR = AethysRotation;
-
+-- Addon
+local addonName, addonTable = ...;
+-- AethysRotation
+local AR = AethysRotation;
 
 --- ============================ CONTENT ============================
-  -- All settings here should be moved into the GUI someday.
-  AR.GUISettings.APL.Shaman = {
-    Enhancement = {
-      --{Display GCD as OffGcd, ForceReturn}
-      GCDasOffGCD = {
-        -- Abilities
-		FeralSpirit = {true, false},
-		Ascendance = {true, false}
-      },
-      --{Display OffGCD as OffGCD, ForceReturn}
-      OffGCDasOffGCD = {
-		-- Abilities
-		DoomWinds = {true, false},
-		-- Interrupt
-		WindShear = {true, false},
-        -- Racial
-        Berserking = {true, false};
-        BloodFury = {true, false}
-      }
-    }
-  }
+-- All settings here should be moved into the GUI someday.
+AR.GUISettings.APL.Shaman = {
+	Enhancement = {
+		-- Display GCD as OffGcd, ForceReturn
+		GCDasOffGCD = {
+			-- Abilities
+			FeralSpirit = {true, false},
+			Ascendance = {true, false}
+		},
+		-- Display OffGCD as OffGCD, ForceReturn
+		OffGCDasOffGCD = {
+			-- Abilities
+			DoomWinds = {true, false},
+
+			-- Interrupt
+			WindShear = {true, false},
+
+			-- Racial
+			Berserking = {true, false},
+			BloodFury = {true, false}
+		}
+	}
+}
