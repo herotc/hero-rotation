8d6dfaa0e31a4d00b2b78b0a3976f000108f964b
chrislopez24 <pagarizabal2@gmail.com>
2017-07-05 13:14:44 +0200

    [Unholy] Pushed the wrong file

--- AethysRotation_DeathKnight/Settings.lua
+++ AethysRotation_DeathKnight/Settings.lua
@@ -6,48 +6,79 @@
   local AR = AethysRotation;
   -- AethysCore
   local AC = AethysCore;
-  -- File Locals
+  --File Locals
   local GUI = AC.GUI;
   local CreateChildPanel = GUI.CreateChildPanel;
   local CreatePanelOption = GUI.CreatePanelOption;
 
+
 --- ============================ CONTENT ============================
   -- All settings here should be moved into the GUI someday.
-  AR.GUISettings.APL.Priest = {
+  AR.GUISettings.APL.DeathKnight = {
     Commons = {
-    },
-    Shadow = {
-      DispersionHP = 10,
       -- {Display GCD as OffGCD, ForceReturn}
       GCDasOffGCD = {
         -- Abilities
-        Shadowfiend = {true, false},
-        Mindbender = {true, false},
-        Shadowform = {true, false}
+
       },
       -- {Display OffGCD as OffGCD, ForceReturn}
       OffGCDasOffGCD = {
+        -- Racials
+        
         -- Abilities
-        PowerInfusion = {true, false},
-        Dispersion = {true, false},
+        
+      }
+    },
+   Frost = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+        
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
         -- Racials
         ArcaneTorrent = {true, false},
         Berserking = {true, false},
         BloodFury = {true, false},
+        -- Abilities
+        PillarOfFrost = {true, false},
+        HungeringRuneWeapon = {true, false},
+        EmpowerRuneWeapon = {true, false},
+        Obliteration = {true, false},
+        
       }
     },
+   Unholy = {
+	  -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+		  
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Racials
+        ArcaneTorrent = {true, false},
+        Berserking = {true, false},
+        BloodFury = {true, false},
+        -- Abilities
+        BlightedRuneWeapon = {true, false},
+		ArmyOfDead = {true, false},
+		SummonGargoyle = {true, false},
+		DarkArbiter = {true, false},
+       }
+     }	 
+	
   };
-
+  
   AR.GUI.LoadSettingsRecursively(AR.GUISettings);
-
-  -- Child Panels
+  -- Panels
   local ARPanel = AR.GUI.Panel;
-  local CP_Priest = CreateChildPanel(ARPanel, "Priest");
-  local CP_Shadow = CreateChildPanel(CP_Priest, "Shadow");
-
-  CreatePanelOption("Slider", CP_Shadow, "APL.Priest.Shadow.DispersionHP", {0, 100, 1}, "Dispersion HP", "Set the Dispersion HP threshold.");
-  CreatePanelOption("CheckButton", CP_Shadow, "APL.Priest.Shadow.GCDasOffGCD.Shadowfiend", "Shadowfiend as Off GCD", "Enable if you want to put Shadowfiend shown as Off GCD (top icons) instead of Main.");
-  CreatePanelOption("CheckButton", CP_Shadow, "APL.Priest.Shadow.GCDasOffGCD.Mindbender", "Mindbender as Off GCD", "Enable if you want to put Mindbender shown as Off GCD (top icons) instead of Main.");
-  CreatePanelOption("CheckButton", CP_Shadow, "APL.Priest.Shadow.GCDasOffGCD.Shadowform", "Shadowform as Off GCD", "Enable if you want to put Shadowform shown as Off GCD (top icons) instead of Main.");
-  CreatePanelOption("CheckButton", CP_Shadow, "APL.Priest.Shadow.OffGCDasOffGCD.PowerInfusion", "Power Infusion as Off GCD", "Enable if you want to put Power Infusion shown as Off GCD (top icons) instead of Main.");
-  CreatePanelOption("CheckButton", CP_Shadow, "APL.Priest.Shadow.OffGCDasOffGCD.Dispersion", "Dispersion Off GCD", "Enable if you want to put Dispersion shown as Off GCD (top icons) instead of Main.");
+  local CP_Deathknight = CreateChildPanel(ARPanel, "DeathKnight");
+  local CP_Unholy = CreateChildPanel(CP_Deathknight, "Unholy");
+  
+  CreatePanelOption("CheckButton", CP_Unholy, "APL.DeathKnight.Unholy.OffGCDasOffGCD.ArmyOfDead", "Army as Off GCD", "Enable if you want to put Army shown as Off GCD (top icons) instead of Main.");
+  CreatePanelOption("CheckButton", CP_Unholy, "APL.DeathKnight.Unholy.OffGCDasOffGCD.SummonGargoyle", "Gargoyle as Off GCD", "Enable if you want to put Gargoyle shown as Off GCD (top icons) instead of Main.");
+  CreatePanelOption("CheckButton", CP_Unholy, "APL.DeathKnight.Unholy.OffGCDasOffGCD.DarkArbiter", "Dark Arbiter as Off GCD", "Enable if you want to put Dark Arbiter shown as Off GCD (top icons) instead of Main.");
+  CreatePanelOption("CheckButton", CP_Unholy, "APL.DeathKnight.Unholy.OffGCDasOffGCD.BlightedRuneWeapon", "Blighted Rune Weapon as Off GCD", "Enable if you want to put Blighted Rune Weapon shown as Off GCD (top icons) instead of Main.");
+  
