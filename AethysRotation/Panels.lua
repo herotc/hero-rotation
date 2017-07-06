8d3a86f0827ebd5a66fe6b033120453d410b9131
Aethys256 <aethys256@users.noreply.github.com>
2017-07-06 11:11:02 +0200

    [Panels] Get the right type for final key

--- AethysRotation/Panels.lua
+++ AethysRotation/Panels.lua
@@ -6,6 +6,7 @@
   local AC = AethysCore;
   -- File Locals
   local CreatePanelOption = AC.GUI.CreatePanelOption;
+  local StringToNumberIfPossible = AC.StringToNumberIfPossible;
 
 
 --- ============================ CONTENT ============================
@@ -26,11 +27,14 @@
         AR.GUI.LoadSettingsRecursively(Value, NewKeyChain);
       -- Update the value
       else
+        -- Check if the final key is a string or a number (the case for table values with numeric index)
+        local ParsedKey = StringToNumberIfPossible(Key);
+        -- Load the saved value
         local DBSetting = AethysRotationDB.GUISettings[NewKeyChain];
-        -- Take the saved value
+        -- If the saved value exists, take it
         if DBSetting ~= nil then
-          Table[Key] = DBSetting;
-        -- Save the default value
+          Table[ParsedKey] = DBSetting;
+        -- Else, save the default value
         else
           AethysRotationDB.GUISettings[NewKeyChain] = Value;
         end
@@ -41,13 +45,13 @@
   local CreateARPanelOption = {
     GCDasOffGCD =
       function (Panel, Setting, Name)
-        CreatePanelOption("CheckButton", Panel, Setting,
+        CreatePanelOption("CheckButton", Panel, Setting .. ".1",
                           Name .. " as Off GCD",
                           "Enable if you want to put " .. Name .. " shown as Off GCD (top icons) instead of Main.");
       end,
     OffGCDasOffGCD = 
       function (Panel, Setting, Name)
-        CreatePanelOption("CheckButton", Panel, Setting,
+        CreatePanelOption("CheckButton", Panel, Setting .. ".1",
                           Name .. " as Off GCD",
                           "Enable if you want to put " .. Name .. " shown as Off GCD (top icons) instead of Main.");
       end
