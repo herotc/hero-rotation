652426d87bc234f450346d8447b0146c669ca149
Chris Lopez <pagarizabal2@gmail.com>
2017-08-30 02:54:00 +0200

    [Frost DeathKnight] typo

--- AethysRotation_DeathKnight/Frost.lua
+++ AethysRotation_DeathKnight/Frost.lua
@@ -410,8 +410,7 @@
     if I.ColdHeart:IsEquipped() and ((Player:BuffStack(S.ChilledHearth) >= 10 and not Player:Buff(S.Obliteration)) or Target:TimeToDie() <= Player:GCD()) then
     --[[COLD HEART LEGENDARY APL]]--
     --actions.cold_heart=chains_of_ice,if=buff.cold_heart.stack=20&buff.unholy_strength.up&cooldown.pillar_of_frost.remains>6
-      if S.ChainsOfIce:IsCastable() and Player:BuffStack(S.ChilledHearth) == 20 and Player:Buff(S.UnholyStrength) and S.PillarOfFrost:CooldownRemains() > 6 then
-        if AR.Cast(S.ChainsOfIce) then return ""; end
+      if S.ChainsOfIce:IsCastable() and Player:BuffStack(S.ChilledHearth) == 20 and Player:Buff(S.UnholyStrength) and S.PillarOfFrost:CooldownRemains() > 6 then        if AR.Cast(S.ChainsOfIce) then return ""; end
       end
     --actions.cold_heart+=/chains_of_ice,if=buff.pillar_of_frost.up&buff.pillar_of_frost.remains<gcd&(buff.cold_heart.stack>=11|(buff.cold_heart.stack>=10&set_bonus.tier20_4pc))
       if S.ChainsOfIce:IsCastable() and Player:Buff(S.PillarOfFrost) and Player:BuffRemains(S.PillarOfFrost) < Player:GCD() and (Player:BuffStack(S.ChilledHearth) >= 11 or (Player:BuffStack(S.ChilledHearth) >= 10 and T204P)) then
