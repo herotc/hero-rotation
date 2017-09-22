cdba1ebbc8f14188fabe600b89758dafbe8a459a
Chris Lopez <pagarizabal2@gmail.com>
2017-09-22 07:14:21 -0700

    [Blood] Added IcyVeins Rotation
    **  no support for defensive cooldowns usage **
    -- You can choose which one will be suggested on settings (false by default)

--- AethysRotation_DeathKnight/Blood.lua
+++ AethysRotation_DeathKnight/Blood.lua
@@ -26,6 +26,7 @@
   ArcaneTorrent					= Spell(50613),
   -- Abilities
   BloodDrinker					= Spell(206931),
+  BloodPlague					= Spell(55078),
   DancingRuneWeaponBuff			= Spell(81256),
   DancingRuneWeapon 			= Spell(49028),
   DeathStrike					= Spell(49998),
@@ -44,6 +45,13 @@
 
 };
 local S = Spell.DeathKnight.Blood;
+
+  -- GUI Settings
+local Settings = {
+   General = AR.GUISettings.General,
+   DeathKnight = AR.GUISettings.APL.DeathKnight
+ };
+
 
 --- ======= ACTION LISTS =======
 local function SingleTarget()
@@ -107,6 +115,62 @@ local function SingleTarget()
 	return false;
 
 end
+local function IcyVeinsRotation()
+	if S.BloodDrinker:IsCastable() and S.BloodDrinker:IsAvailable() and not Player:Buff(S.DancingRuneWeaponBuff) then
+		if AR.Cast(S.BloodDrinker) then return ""; end
+	end
+
+	if S.DancingRuneWeapon:IsCastable() then
+		if AR.Cast(S.DancingRuneWeapon) then return ""; end
+	end
+
+	if S.Marrowrend:IsCastable() and Player:BuffRemains(S.BoneShield) <= 3 then
+		if AR.Cast(S.Marrowrend) then return ""; end
+	end
+
+	if S.BloodBoil:IsCastable() and Cache.EnemiesCount[10] >= 1 and not Target:Debuff(S.BloodPlague) then
+		if AR.Cast(S.BloodBoil) then return ""; end
+	end
+
+	if S.DeathandDecay:IsCastable() and (Cache.EnemiesCount[10] == 1 and Player:Buff(S.CrimsonScourge) and S.RapidDecomposition:IsAvailable()) or (Cache.EnemiesCount[10] > 1 and Player:Buff(S.CrimsonScourge)) then
+		if AR.Cast(S.DeathandDecay) then return ""; end
+	end
+
+	if S.DeathStrike:IsUsable() and Player:RunicPowerDeficit() >= 10 then
+		if AR.Cast(S.DeathStrike) then return ""; end
+	end
+
+	if S.Marrowrend:IsCastable() and Player:BuffStack(S.BoneShield) <= 6 or (Player:BuffStack(S.BoneShield) < 5 and S.Ossuary:IsAvailable()) then
+		if AR.Cast(S.Marrowrend) then return ""; end
+	end
+
+	if S.DeathandDecay:IsCastable() and (Cache.EnemiesCount[10] == 1 and Player:Runes() >= 3 and S.RapidDecomposition:IsAvailable()) or Cache.EnemiesCount[10] >= 3 then
+		if AR.Cast(S.DeathandDecay) then return ""; end
+	end
+
+	if S.DeathStrike:IsUsable() and ((Player:RunicPower() > (80 or (90 and S.Ossuary:IsAvailable()))  and S.BoneStorm:IsAvailable() and not Player:Buff(S.BoneStorm) and (S.BoneStorm:CooldownRemains() > 15 or ((Player:Runes() > 3 and Player:RunicPowerDeficit() < 15) and S.BoneStorm:CooldownRemains() > 5))) or not S.BoneStorm:IsAvailable()) 
+		and (not S.Ossuary:IsAvailable() or Player:BuffStack(S.BoneShield) > 5 or Player:Runes() >= 3 and Player:RunicPowerDeficit() < 10) then
+		if AR.Cast(S.DeathStrike) then return ""; end
+	end
+
+	if S.HeartStrike:IsCastable() and Player:Runes() >= 3 then
+		if AR.Cast(S.HeartStrike) then return ""; end
+	end
+
+	if S.DeathandDecay:IsCastable() and Player:Buff(S.CrimsonScourge) and not S.RapidDecomposition:IsAvailable() then
+		if AR.Cast(S.DeathandDecay) then return ""; end
+	end
+
+	if S.Consumption:IsCastable() then 
+		if AR.Cast(S.Consumption) then return ""; end
+	end
+
+	if S.BloodBoil:IsCastable() then
+		if AR.Cast(S.BloodBoil) then return ""; end
+	end
+
+	return false;
+end
 
 --- ======= MAIN =======
 local function APL ()
@@ -116,11 +180,15 @@ local function APL ()
 
    -- In Combat
     if Everyone.TargetIsValid() and Target:IsInRange(20) then
-  
-      ShouldReturn = SingleTarget();
-      if ShouldReturn then return ShouldReturn;
-      end 
-  end
-  return;
+    	if Settings.DeathKnight.Blood.useIcyVeinsRotation then
+			ShouldReturn = IcyVeinsRotation();
+		else 
+			ShouldReturn = SingleTarget();
+      		if ShouldReturn then return ShouldReturn;
+      		end
+      	end 
+  	end
+
+  	return;
 end
 AR.SetAPL(250,APL);
