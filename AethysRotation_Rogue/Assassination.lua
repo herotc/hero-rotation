ef6b2af4e34d7d2bd1bf89f65db31aded4e6ade0
Aethys256 <aethys256@users.noreply.github.com>
2017-10-14 13:26:34 +0200

    [Assassination] Reduce cache garbage

--- AethysRotation_Rogue/Assassination.lua
+++ AethysRotation_Rogue/Assassination.lua
@@ -178,14 +178,20 @@ local pairs = pairs;
 
 -- APL Action Lists (and Variables)
 -- actions=variable,name=energy_regen_combined,value=energy.regen+poisoned_bleeds*(7+talent.venom_rush.enabled*3)%2
-local function Energy_Regen_Combined ()
-  return Cache.Get("APLVar", "Energy_Regen_Combined",
-                   function() return Player:EnergyRegen() + Rogue.PoisonedBleeds() * (7 + (S.VenomRush:IsAvailable() and 3 or 0)) / 2; end);
+local Energy_Regen_Combined;
+do
+  local _Energy_Regen_Combined = function() return Player:EnergyRegen() + Rogue.PoisonedBleeds() * (7 + (S.VenomRush:IsAvailable() and 3 or 0)) / 2; end;
+  Energy_Regen_Combined = function ()
+    return Cache.Get("APLVar", "Energy_Regen_Combined", _Energy_Regen_Combined);
+  end
 end
 -- actions+=/variable,name=energy_time_to_max_combined,value=energy.deficit%variable.energy_regen_combined
-local function Energy_Time_To_Max_Combined ()
-  return Cache.Get("APLVar", "Energy_Time_To_Max_Combined",
-                   function() return Player:EnergyDeficit() / Energy_Regen_Combined(); end);
+local Energy_Time_To_Max_Combined;
+do
+  local _Energy_Time_To_Max_Combined = function() return Player:EnergyDeficit() / Energy_Regen_Combined(); end;
+  Energy_Time_To_Max_Combined = function ()
+    return Cache.Get("APLVar", "Energy_Regen_Combined", _Energy_Time_To_Max_Combined);
+  end
 end
 -- # Builders
 local function Build ()
