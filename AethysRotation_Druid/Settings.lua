847a95646dfd922739432a42dd7b653cd6eb8b01
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2017-10-16 11:11:30 +0200

    [Druid] Setting format

--- AethysRotation_Druid/Settings.lua
+++ AethysRotation_Druid/Settings.lua
@@ -9,6 +9,37 @@
 --- ============================ CONTENT ============================
   -- All settings here should be moved into the GUI someday.
   AR.GUISettings.APL.Druid = {
+    Commons = {
+      
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Racials
+        Racials = {true, false}
+        -- Abilities
+        
+      }
+    },
+    Balance = {
+      -- {Display GCD as OffGCD, ForceReturn}
+      GCDasOffGCD = {
+        -- Abilities
+          MoonkinForm = {true, false}
+      },
+      -- {Display OffGCD as OffGCD, ForceReturn}
+      OffGCDasOffGCD = {
+        -- Abilities
+          BlessingofElune = {true, false},
+          BlessingofAnshe = {true, false},
+          AstralCommunion = {true, false},
+          IncarnationChosenOfElune = {true, false},
+          CelestialAlignment = {true, false},
+          WarriorofElune = {true, false},
+      }
+    },
     Feral = {
       -- {Display GCD as OffGCD, ForceReturn}
       GCDasOffGCD = {
@@ -17,26 +48,23 @@
       OffGCDasOffGCD = {
       }
     },
-    Balance = {
+    Guardian = {
       -- {Display GCD as OffGCD, ForceReturn}
       GCDasOffGCD = {
-		-- Abilities
-        MoonkinForm = {true, false}
       },
       -- {Display OffGCD as OffGCD, ForceReturn}
       OffGCDasOffGCD = {
-		-- Abilities
-        BlessingofElune = {true, false},
-        BlessingofAnshe = {true, false},
-        AstralCommunion = {true, false},
-        IncarnationChosenOfElune = {true, false},
-        CelestialAlignment = {true, false},
-		WarriorofElune = {true, false},
-		
-		--Racials
-		ArcaneTorrent = {true, false},
-        Berserking = {true, false},
-        BloodFury = {true, false},
       }
-    }
+    },
   };
+
+    AR.GUI.LoadSettingsRecursively(AR.GUISettings);
+
+  -- Child Panels
+  local ARPanel = AR.GUI.Panel;
+  local CP_Druid = CreateChildPanel(ARPanel, "Druid");
+  local CP_Balance = CreateChildPanel(CP_Druid, "Balance");
+  -- local CP_Feral = CreateChildPanel(CP_Druid, "Feral");
+  -- local CP_Guardian = CreateChildPanel(CP_Druid, "Guardian");
+
+  CreateARPanelOption("OffGCDasOffGCD", CP_Druid, "APL.Druid.Commons.OffGCDasOffGCD.Racials", "Racials");
