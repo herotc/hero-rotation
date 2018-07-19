fa063fe96330168e5429f4b97356347a78cfbf90
Nia <Nianel@users.noreply.github.com>
2018-07-19 16:30:29 +0200

    [Fury] Add potions and change settings

--- HeroRotation_Warrior/Fury.lua
+++ HeroRotation_Warrior/Fury.lua
@@ -101,8 +101,8 @@ local HR = HeroRotation;
   -- # single_target
   local function single_target ()
     -- actions.single_target=siegebreaker,if=buff.recklessness.up|cooldown.recklessness.remains>28
-    if S.Siegebreaker:IsReady() and S.Siegebreaker:IsAvailable() and (Player:Buff(S.Recklessness) or S.Recklessness:CooldownRemainsP() > 28 ) then
-      if HR.Cast(S.Siegebreaker) then return ""; end
+    if HR.CDsON() and S.Siegebreaker:IsReady() and S.Siegebreaker:IsAvailable() and (Player:Buff(S.Recklessness) or S.Recklessness:CooldownRemainsP() > 28 ) then
+      if HR.Cast(S.Siegebreaker, Settings.Fury.GCDasOffGCD.Siegebreaker) then return ""; end
     end
     -- actions.single_target+=/rampage,if=buff.recklessness.up|(talent.frothing_berserker.enabled|talent.carnage.enabled&(buff.enrage.remains<gcd|rage>90)|talent.massacre.enabled&(buff.enrage.remains<gcd|rage>90))
     if S.Rampage:IsReady() and (Player:Buff(S.Recklessness) or (S.FrothingBerserker:IsAvailable() or S.Carnage:IsAvailable() and (Player:BuffRemainsP(S.Enrage) < Player:GCD() or Player:Rage() > 90) or S.Massacre:IsAvailable() and (Player:BuffRemainsP(S.Enrage) < Player:GCD() or Player:Rage() > 90))) then
@@ -126,14 +126,14 @@ local HR = HeroRotation;
     end
     -- actions.single_target+=/bladestorm,if=prev_gcd.1.rampage&(debuff.siegebreaker.up|!talent.siegebreaker.enabled)
     if HR.CDsON() and S.Bladestorm:IsCastable() and Player:PrevGCDP(1, S.Rampage) and (Target:Debuff(S.SiegebreakerDebuff) or not S.Siegebreaker:IsAvailable()) then
-      if HR.Cast(S.Bladestorm) then return ""; end
+      if HR.Cast(S.Bladestorm, Settings.Fury.GCDasOffGCD.Bladestorm) then return ""; end
     end
     -- actions.single_target+=/dragon_roar,if=buff.enrage.up&(debuff.siegebreaker.up|!talent.siegebreaker.enabled)
     if HR.CDsON() and S.DragonRoar:IsCastable() and Player:Buff(S.Enrage) and (Target:Debuff(S.SiegebreakerDebuff) or not S.Siegebreaker:IsAvailable()) then
       if HR.Cast(S.DragonRoar, Settings.Fury.GCDasOffGCD.DragonRoar) then return ""; end
     end
     -- actions.single_target+=/raging_blow,if=talent.carnage.enabled|(talent.massacre.enabled&rage<80)|(talent.frothing_berserker.enabled&rage<90)
-    if S.RagingBlow:IsReady() and S.Carnage:IsAvailable() or (S.Massacre:IsAvailable() and Player:Rage() < 80) or (S.FrothingBerserker:IsAvailable() and Player:Rage() < 90) then
+    if S.RagingBlow:IsReady() and (S.Carnage:IsAvailable() or (S.Massacre:IsAvailable() and Player:Rage() < 80) or (S.FrothingBerserker:IsAvailable() and Player:Rage() < 90)) then
       if HR.Cast(S.RagingBlow) then return ""; end
     end
     -- actions.single_target+=/furious_slash,if=talent.furious_slash.enabled
@@ -169,6 +169,12 @@ local function APL ()
   -- actions+=/run_action_list,name=movement,if=movement.distance>5
   -- actions+=/heroic_leap,if=(raid_event.movement.distance>25&raid_event.movement.in>45)|!raid_event.movement.exists
   -- actions+=/potion
+  if Settings.Fury.ShowPoPP and I.PotionOfProlongedPower:IsReady() then
+    if HR.CastSuggested(I.PotionOfProlongedPower) then return ""; end
+  end
+  if Settings.Fury.ShowPoOW and I.PotionoftheOldWar:IsReady() then
+    if HR.CastSuggested(I.PotionoftheOldWar) then return ""; end
+  end
   -- actions+=/furious_slash,if=talent.furious_slash.enabled&(buff.furious_slash.stack<3|buff.furious_slash.remains<3|(cooldown.recklessness.remains<3&buff.furious_slash.remains<9))
   if S.FuriousSlash:IsCastable() and S.FuriousSlash:IsAvailable() and (Player:BuffStack(S.FuriousSlashBuff) < 3 or Player:BuffRemainsP(S.FuriousSlashBuff) < 3 or (S.Recklessness:CooldownRemainsP() < 3 and Player:BuffRemainsP(S.FuriousSlashBuff) < 9)) then
     if HR.Cast(S.FuriousSlash) then return ""; end
@@ -183,7 +189,7 @@ local function APL ()
   end
   -- actions+=/recklessness
   if HR.CDsON() and S.Recklessness:IsCastable() then
-    if HR.Cast(S.Recklessness) then return ""; end
+    if HR.Cast(S.Recklessness, Settings.Fury.GCDasOffGCD.Recklessness) then return ""; end
   end
   -- actions+=/whirlwind,if=spell_targets.whirlwind>1&!buff.meat_cleaver.up
   if HR.AoEON() and S.Whirlwind:IsCastable() and Cache.EnemiesCount[8] > 1 and not Player:Buff(S.MeatCleaver) then
@@ -224,7 +230,7 @@ local function APL ()
 end
 HR.SetAPL(72, APL);
 
---- Last Update: 11/26/2017
+--- Last Update: 07/19/2018
 
 -- # Executed every time the actor is available.
 -- actions=auto_attack
@@ -232,73 +238,28 @@ HR.SetAPL(72, APL);
 -- # This is mostly to prevent cooldowns from being accidentally used during movement.
 -- actions+=/run_action_list,name=movement,if=movement.distance>5
 -- actions+=/heroic_leap,if=(raid_event.movement.distance>25&raid_event.movement.in>45)|!raid_event.movement.exists
--- actions+=/potion,name=old_war,if=buff.battle_cry.up&(buff.avatar.up|!talent.avatar.enabled)
--- actions+=/dragon_roar,if=(equipped.convergence_of_fates&cooldown.battle_cry.remains<2)|!equipped.convergence_of_fates&(!cooldown.battle_cry.remains<=10|cooldown.battle_cry.remains<2)|(talent.bloodbath.enabled&(cooldown.bloodbath.remains<1|buff.bloodbath.up))
--- actions+=/rampage,if=cooldown.battle_cry.remains<1&cooldown.bloodbath.remains<1&target.health.pct>20
--- actions+=/furious_slash,if=talent.frenzy.enabled&(buff.frenzy.stack<3|buff.frenzy.remains<3|(cooldown.battle_cry.remains<1&buff.frenzy.remains<9))
--- actions+=/bloodthirst,if=equipped.kazzalax_fujiedas_fury&buff.fujiedas_fury.down
--- actions+=/avatar,if=((buff.battle_cry.remains>5|cooldown.battle_cry.remains<12)&target.time_to_die>80)|((target.time_to_die<40)&(buff.battle_cry.remains>6|cooldown.battle_cry.remains<12|(target.time_to_die<20)))
--- actions+=/use_item,name=umbral_moonglaives,if=equipped.umbral_moonglaives&(cooldown.battle_cry.remains>gcd&cooldown.battle_cry.remains<2|cooldown.battle_cry.remains=0)
--- actions+=/battle_cry,if=gcd.remains=0&talent.reckless_abandon.enabled&!talent.bloodbath.enabled&(equipped.umbral_moonglaives&(prev_off_gcd.umbral_moonglaives|(trinket.cooldown.remains>3&trinket.cooldown.remains<90))|!equipped.umbral_moonglaives)
--- actions+=/battle_cry,if=gcd.remains=0&talent.bladestorm.enabled&(raid_event.adds.in>90|!raid_event.adds.exists|spell_targets.bladestorm_mh>desired_targets)
--- actions+=/battle_cry,if=gcd.remains=0&buff.dragon_roar.up&(cooldown.bloodthirst.remains=0|buff.enrage.remains>cooldown.bloodthirst.remains)
--- actions+=/battle_cry,if=(gcd.remains=0|gcd.remains<=0.4&prev_gcd.1.rampage)&(cooldown.bloodbath.remains=0|buff.bloodbath.up|!talent.bloodbath.enabled|(target.time_to_die<12))
--- actions+=/bloodbath,if=buff.battle_cry.up|(target.time_to_die<14)|(cooldown.battle_cry.remains<2&prev_gcd.1.rampage)
--- actions+=/blood_fury,if=buff.battle_cry.up
--- actions+=/berserking,if=(buff.battle_cry.up&(buff.avatar.up|!talent.avatar.enabled))|(buff.battle_cry.up&target.time_to_die<40)
--- actions+=/arcane_torrent,if=rage<rage.max-40
--- actions+=/run_action_list,name=cooldowns,if=buff.battle_cry.up&spell_targets.whirlwind=1
--- actions+=/run_action_list,name=three_targets,if=target.health.pct>20&(spell_targets.whirlwind=3|spell_targets.whirlwind=4)
--- actions+=/run_action_list,name=aoe,if=spell_targets.whirlwind>4
--- actions+=/run_action_list,name=execute,if=target.health.pct<20
--- actions+=/run_action_list,name=single_target,if=target.health.pct>20
-
--- actions.aoe=bloodthirst,if=buff.enrage.down|rage<90
--- actions.aoe+=/bladestorm,if=buff.enrage.remains>2&(raid_event.adds.in>90|!raid_event.adds.exists|spell_targets.bladestorm_mh>desired_targets)
--- actions.aoe+=/whirlwind,if=buff.meat_cleaver.down
--- actions.aoe+=/rampage,if=buff.meat_cleaver.up&(buff.enrage.down&!talent.frothing_berserker.enabled|buff.massacre.react|rage>=100)
--- actions.aoe+=/bloodthirst
--- actions.aoe+=/whirlwind
-
--- actions.cooldowns=rampage,if=buff.massacre.react&buff.enrage.remains<1
--- actions.cooldowns+=/bloodthirst,if=target.health.pct<20&buff.enrage.remains<1
--- actions.cooldowns+=/execute
--- actions.cooldowns+=/raging_blow,if=talent.inner_rage.enabled&buff.enrage.up
--- actions.cooldowns+=/rampage,if=(rage>=100&talent.frothing_berserker.enabled&!set_bonus.tier21_4pc)|set_bonus.tier21_4pc|!talent.frothing_berserker.enabled
--- actions.cooldowns+=/odyns_fury,if=buff.enrage.up&(cooldown.raging_blow.remains>0|!talent.inner_rage.enabled)
--- actions.cooldowns+=/berserker_rage,if=talent.outburst.enabled&buff.enrage.down&buff.battle_cry.up
--- actions.cooldowns+=/bloodthirst,if=(buff.enrage.remains<1&!talent.outburst.enabled)|!talent.inner_rage.enabled
--- actions.cooldowns+=/whirlwind,if=buff.wrecking_ball.react&buff.enrage.up
--- actions.cooldowns+=/raging_blow
--- actions.cooldowns+=/bloodthirst
--- actions.cooldowns+=/furious_slash
-
--- actions.execute=bloodthirst,if=buff.fujiedas_fury.up&buff.fujiedas_fury.remains<2
--- actions.execute+=/execute,if=artifact.juggernaut.enabled&(!buff.juggernaut.up|buff.juggernaut.remains<2)|buff.stone_heart.react
--- actions.execute+=/furious_slash,if=talent.frenzy.enabled&buff.frenzy.remains<=2
--- actions.execute+=/rampage,if=buff.massacre.react&buff.enrage.remains<1
--- actions.execute+=/execute
--- actions.execute+=/odyns_fury
--- actions.execute+=/bloodthirst
--- actions.execute+=/furious_slash,if=set_bonus.tier19_2pc
--- actions.execute+=/raging_blow
--- actions.execute+=/furious_slash
+-- actions+=/potion
+-- actions+=/furious_slash,if=talent.furious_slash.enabled&(buff.furious_slash.stack<3|buff.furious_slash.remains<3|(cooldown.recklessness.remains<3&buff.furious_slash.remains<9))
+-- actions+=/bloodthirst,if=equipped.kazzalax_fujiedas_fury&(buff.fujiedas_fury.down|remains<2)
+-- actions+=/rampage,if=cooldown.recklessness.remains<3
+-- actions+=/recklessness
+-- actions+=/whirlwind,if=spell_targets.whirlwind>1&!buff.meat_cleaver.up
+-- actions+=/blood_fury,if=buff.recklessness.up
+-- actions+=/berserking,if=buff.recklessness.up
+-- actions+=/arcane_torrent,if=rage<40&!buff.recklessness.up
+-- actions+=/lights_judgment,if=cooldown.recklessness.remains<3
+-- actions+=/run_action_list,name=single_target
 
 -- actions.movement=heroic_leap
 
--- actions.single_target=bloodthirst,if=buff.fujiedas_fury.up&buff.fujiedas_fury.remains<2
--- actions.single_target+=/furious_slash,if=talent.frenzy.enabled&(buff.frenzy.down|buff.frenzy.remains<=2)
--- actions.single_target+=/raging_blow,if=buff.enrage.up&talent.inner_rage.enabled
--- actions.single_target+=/rampage,if=target.health.pct>21&(rage>=100|!talent.frothing_berserker.enabled)&(((cooldown.battle_cry.remains>5|cooldown.bloodbath.remains>5)&!talent.carnage.enabled)|((cooldown.battle_cry.remains>3|cooldown.bloodbath.remains>3)&talent.carnage.enabled))|buff.massacre.react
--- actions.single_target+=/execute,if=buff.stone_heart.react&((talent.inner_rage.enabled&cooldown.raging_blow.remains>1)|buff.enrage.up)
+-- actions.single_target=siegebreaker,if=buff.recklessness.up|cooldown.recklessness.remains>28
+-- actions.single_target+=/rampage,if=buff.recklessness.up|(talent.frothing_berserker.enabled|talent.carnage.enabled&(buff.enrage.remains<gcd|rage>90)|talent.massacre.enabled&(buff.enrage.remains<gcd|rage>90))
+-- actions.single_target+=/execute,if=buff.enrage.up
+-- actions.single_target+=/bloodthirst,if=buff.enrage.down
+-- actions.single_target+=/raging_blow,if=charges=2
 -- actions.single_target+=/bloodthirst
--- actions.single_target+=/furious_slash,if=set_bonus.tier19_2pc&!talent.inner_rage.enabled
--- actions.single_target+=/whirlwind,if=buff.wrecking_ball.react&buff.enrage.up
--- actions.single_target+=/raging_blow
--- actions.single_target+=/furious_slash
-
--- actions.three_targets=execute,if=buff.stone_heart.react
--- actions.three_targets+=/rampage,if=buff.meat_cleaver.up&((buff.enrage.down&!talent.frothing_berserker.enabled)|(rage>=100&talent.frothing_berserker.enabled))|buff.massacre.react
--- actions.three_targets+=/raging_blow,if=talent.inner_rage.enabled
--- actions.three_targets+=/bloodthirst
--- actions.three_targets+=/whirlwind
+-- actions.single_target+=/bladestorm,if=prev_gcd.1.rampage&(debuff.siegebreaker.up|!talent.siegebreaker.enabled)
+-- actions.single_target+=/dragon_roar,if=buff.enrage.up&(debuff.siegebreaker.up|!talent.siegebreaker.enabled)
+-- actions.single_target+=/raging_blow,if=talent.carnage.enabled|(talent.massacre.enabled&rage<80)|(talent.frothing_berserker.enabled&rage<90)
+-- actions.single_target+=/furious_slash,if=talent.furious_slash.enabled
+-- actions.single_target+=/whirlwind
