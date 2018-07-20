00f9df8d3d775a91f6f799aaed706d050361baa3
Nia <Nianel@users.noreply.github.com>
2018-07-20 02:12:42 +0200

    [Survival] Fix wildfire bomb

--- HeroRotation_Hunter/Survival.lua
+++ HeroRotation_Hunter/Survival.lua
@@ -55,14 +55,20 @@
     HydrasBite                    = Spell(260241),
     MongooseBite                  = Spell(259387),
     MongooseFury                  = Spell(259388),
-    ShrapnelBomb                  = Spell(270335),
     SteelTrap                     = Spell(162488),
     SteelTrapDot                  = Spell(162487),
     TermsofEngagement             = Spell(265895),
     TipoftheSpear                 = Spell(260285),
     TipoftheSpearBuff             = Spell(260286),
     VipersVenom                   = Spell(268501),
+    VipersVenomBuff               = Spell(268552),
     WildfireInfusion              = Spell(271014),
+    ShrapnelBomb                  = Spell(270335),
+    ShrapnelBombDot               = Spell(270339),
+    VolatileBomb                  = Spell(271045),
+    VolatileBombDot               = Spell(271049),
+    PheromoneBomb                 = Spell(270323),
+    PheromoneBombDot              = Spell(270332),
     -- Defensive
     AspectoftheTurtle             = Spell(186265),
     Exhilaration                  = Spell(109304),
@@ -191,7 +197,14 @@
         if HR.Cast(S.KillCommand) then return ""; end
       end
       -- actions+=/wildfire_bomb,if=(focus+cast_regen<focus.max|active_enemies>1)&(dot.wildfire_bomb.refreshable&buff.mongoose_fury.down|full_recharge_time<gcd)
-      if Target:IsInRange(40) and S.WildfireBomb:IsCastable() and ((Player:Focus() + Player:FocusCastRegen (Player:GCD()) < Player:FocusMax() or Cache.EnemiesCount[5] > 1) and (Target:DebuffRefreshable(S.WildfireBombDot, 1.8) and not Player:Buff(S.MongooseFury) or S.WildfireBomb:FullRechargeTime() < Player:GCD())) then
+      if Target:IsInRange(40) and S.WildfireBomb:IsCastable() and not S.WildfireInfusion:IsAvailable() and ((Player:Focus() + Player:FocusCastRegen (Player:GCD()) < Player:FocusMax() or Cache.EnemiesCount[5] > 1) and (not Target:Debuff(S.WildfireBombDot) and not Player:Buff(S.MongooseFury) or S.WildfireBomb:FullRechargeTime() < Player:GCD())) then
+        if HR.Cast(S.WildfireBomb) then return ""; end
+      end
+      -- add ShrapnelBomb PheromoneBomb and VolatileBomb for the talent WildfireInfusion
+      if Target:IsInRange(40) and (S.WildfireInfusion:IsAvailable() and (S.ShrapnelBomb:IsCastable() or S.PheromoneBomb:IsCastable() or S.VolatileBomb:IsCastable())) 
+      and ((Player:Focus() + Player:FocusCastRegen (Player:GCD()) < Player:FocusMax() or Cache.EnemiesCount[5] > 1) 
+      and ((not Target:Debuff(S.ShrapnelBombDot) or not Target:Debuff(S.PheromoneBombDot) or not Target:Debuff(S.VolatileBombDot)) 
+      and not Player:Buff(S.MongooseFury) or (S.ShrapnelBomb:FullRechargeTime() or S.PheromoneBomb:FullRechargeTime() or S.VolatileBomb:FullRechargeTime()) < Player:GCD())) then
         if HR.Cast(S.WildfireBomb) then return ""; end
       end
       -- actions+=/kill_command,target_if=min:bloodseeker.remains,if=focus+cast_regen<focus.max&buff.tip_of_the_spear.stack<3
@@ -199,11 +212,11 @@
         if HR.Cast(S.KillCommand) then return ""; end
       end
       -- actions+=/butchery,if=(!talent.wildfire_infusion.enabled|full_recharge_time<gcd)&active_enemies>3|(dot.shrapnel_bomb.ticking&dot.internal_bleeding.stack<3)
-      if S.Butchery:IsCastable() and Player:FocusPredicted(0.2) > 30 and (not S.WildfireInfusion:IsAvailable() or S.Butchery:FullRechargeTime() < Player:GCD()) and Cache.EnemiesCount[8] > 3 or (Target:Debuff(S.ShrapnelBomb) and Target:DebuffStack(S.InternalBleeding) < 3) then
+      if S.Butchery:IsAvailable() and S.Butchery:IsCastable() and Player:FocusPredicted(0.2) > 30 and ((not S.WildfireInfusion:IsAvailable() or S.Butchery:FullRechargeTime() < Player:GCD()) and Cache.EnemiesCount[8] > 3 or (Target:Debuff(S.ShrapnelBombDot) and Target:DebuffStack(S.InternalBleeding) < 3)) then
         if HR.Cast(S.Butchery) then return ""; end
       end
       -- -- actions+=/serpent_sting,if=(active_enemies<2&refreshable&(buff.mongoose_fury.down|(variable.can_gcd&!talent.vipers_venom.enabled)))|buff.vipers_venom.up
-      if S.SerpentSting:IsCastable() and Target:IsInRange(40) and Player:FocusPredicted(0.2) > 20 and (Cache.EnemiesCount[8] < 2 and Target:DebuffRefreshable(S.SerpentSting, 2.7) and (not Player:Buff(S.MongooseFury) or (CanGCD() and not S.VipersVenom:IsAvailable()))) or Player:Buff(S.VipersVenom) then
+      if S.SerpentSting:IsCastable() and Target:IsInRange(40) and Player:FocusPredicted(0.2) > 20 and ((Cache.EnemiesCount[8] < 2 and Target:DebuffRefreshable(S.SerpentSting, 2.7) and (not Player:Buff(S.MongooseFury) or (CanGCD() and not S.VipersVenom:IsAvailable()))) or Player:Buff(S.VipersVenomBuff)) then
         if HR.Cast(S.SerpentSting) then return ""; end
       end
       -- actions+=/carve,if=active_enemies>2&(active_enemies<6&active_enemies+gcd<cooldown.wildfire_bomb.remains|5+gcd<cooldown.wildfire_bomb.remains)
@@ -223,15 +236,15 @@
         if HR.Cast(S.Chakrams) then return ""; end
       end
       -- actions+=/serpent_sting,target_if=min:remains,if=refreshable&buff.mongoose_fury.down|buff.vipers_venom.up
-      if S.SerpentSting:IsCastable() and Target:IsInRange(40) and Player:FocusPredicted(0.2) > 20 and Target:DebuffRefreshable(S.SerpentSting, 2.7) and ( not Player:Buff(S.MongooseFury) or Player:Buff(S.VipersVenom)) then
+      if S.SerpentSting:IsCastable() and Target:IsInRange(40) and Player:FocusPredicted(0.2) > 20 and (Target:DebuffRefreshable(S.SerpentSting, 2.7) and (not Player:Buff(S.MongooseFury) or Player:Buff(S.VipersVenom))) then
         if HR.Cast(S.SerpentSting) then return ""; end
       end
       -- actions+=/mongoose_bite,target_if=min:dot.internal_bleeding.stack,if=buff.mongoose_fury.up|focus>60
-      if S.MongooseBite:IsCastable() and (Player:Buff(S.MongooseFury) or Player:Focus() > 60) then
+      if S.MongooseBite:IsCastable() and S.MongooseBite:IsAvailable() and Player:FocusPredicted(0.2) > 30 and (Player:Buff(S.MongooseFury) or Player:Focus() > 60) then
         if HR.Cast(S.MongooseBite) then return ""; end
       end
       -- actions+=/butchery
-      if S.Butchery:IsCastable() and Player:FocusPredicted(0.2) > 30 then
+      if S.Butchery:IsCastable() and S.Butchery:IsAvailable() and Player:FocusPredicted(0.2) > 30 then
         if HR.Cast(S.Butchery) then return ""; end
       end
       -- actions+=/raptor_strike,target_if=min:dot.internal_bleeding.stack
