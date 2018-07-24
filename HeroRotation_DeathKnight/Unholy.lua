90a863ee168a4f94a4574568d45c4064064f92bb
Krich <pagarizabal2@gmail.com>
2018-07-25 00:50:58 +0200

    [UH] and this should fix aotd st

--- HeroRotation_DeathKnight/Unholy.lua
+++ HeroRotation_DeathKnight/Unholy.lua
@@ -197,11 +197,11 @@ end
     if HR.Cast(S.Defile) then return ""; end
   end
   -- scourge_strike,if=((debuff.festering_wound.up&cooldown.apocalypse.remains>5)|debuff.festering_wound.stack>4)&cooldown.army_of_the_dead.remains>5
-  if S.ScourgeStrike:IsCastable() and (((Target:Debuff(S.FesteringWound) and S.Apocalypse:CooldownRemainsP() > 5) or Target:DebuffStack(S.FesteringWound) > 4) and S.ArmyOfTheDead:CooldownRemainsP() > 5) then
+  if S.ScourgeStrike:IsCastable() and (((Target:Debuff(S.FesteringWound) and S.Apocalypse:CooldownRemainsP() > 5) or Target:DebuffStack(S.FesteringWound) > 4) and (S.ArmyOfTheDead:CooldownRemainsP() > 5 or S.ArmyOfTheDead:IsCastable())) then
     if HR.Cast(S.ScourgeStrike) then return ""; end
   end
   -- clawing_shadows,if=((debuff.festering_wound.up&cooldown.apocalypse.remains>5)|debuff.festering_wound.stack>4)&cooldown.army_of_the_dead.remains>5
-  if S.ClawingShadows:IsCastable() and (((Target:Debuff(S.FesteringWound) and S.Apocalypse:CooldownRemainsP() > 5) or Target:DebuffStack(S.FesteringWound) > 4) and S.ArmyOfTheDead:CooldownRemainsP() > 5) then
+  if S.ClawingShadows:IsCastable() and (((Target:Debuff(S.FesteringWound) and S.Apocalypse:CooldownRemainsP() > 5) or Target:DebuffStack(S.FesteringWound) > 4) and (S.ArmyOfTheDead:CooldownRemainsP() > 5 or S.ArmyOfTheDead:IsCastable())) then
     if HR.Cast(S.ClawingShadows) then return ""; end
   end
   -- death_coil,if=runic_power.deficit<20&!variable.pooling_for_gargoyle
@@ -209,7 +209,7 @@ end
     if HR.Cast(S.DeathCoil) then return ""; end
   end
   -- festering_strike,if=((((debuff.festering_wound.stack<4&!buff.unholy_frenzy.up)|debuff.festering_wound.stack<3)&cooldown.apocalypse.remains<3)|debuff.festering_wound.stack<1)&cooldown.army_of_the_dead.remains>5
-  if S.FesteringStrike:IsCastable() and (((((Target:DebuffStack(S.FesteringWound) < 4 and not Player:Buff(S.UnholyFrenzy)) or Target:DebuffStack(S.FesteringWound) < 3) and S.Apocalypse:CooldownRemainsP() < 3) or Target:DebuffStack(S.FesteringWound) < 1) and S.ArmyOfTheDead:CooldownRemainsP() > 5) then
+  if S.FesteringStrike:IsCastable() and (((((Target:DebuffStack(S.FesteringWound) < 4 and not Player:Buff(S.UnholyFrenzy)) or Target:DebuffStack(S.FesteringWound) < 3) and S.Apocalypse:CooldownRemainsP() < 3) or Target:DebuffStack(S.FesteringWound) < 1) and (S.ArmyOfTheDead:CooldownRemainsP() > 5 or S.ArmyOfTheDead:IsCastable())) then
     if HR.Cast(S.FesteringStrike) then return ""; end
   end
   -- death_coil,if=!variable.pooling_for_gargoyle
