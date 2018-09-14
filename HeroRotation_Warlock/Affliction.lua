b884e5574ca146ae5224f74b317ca24a7fcb6d24
ghr74 <28192634+ghr74@users.noreply.github.com>
2018-09-14 19:05:31 +0200

    [Affliction] Register InFlight

--- HeroRotation_Warlock/Affliction.lua
+++ HeroRotation_Warlock/Affliction.lua
@@ -136,10 +136,13 @@ end
 local function TimeToShard()
     local agony_count = NbAffected(S.Agony)
     if agony_count == 0 then
-        return 10000 
+        return 10000
     end
     return 1 / (0.16 / math.sqrt(agony_count) * (agony_count == 1 and 1.15 or 1) * agony_count / S.Agony:TickTime())
 end
+
+S.ShadowBolt:RegisterInFlight()
+S.S.SeedofCorruption:RegisterInFlight()
 --- ======= ACTION LISTS =======
 local function APL()
   local Precombat, Fillers
