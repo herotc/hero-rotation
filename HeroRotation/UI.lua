e2bedd1c4fb22051c46b12daf899c29df05649de
ghr74 <28192634+ghr74@users.noreply.github.com>
2019-05-20 21:37:20 +0200

    [UI] Use different ElvUI nameplate enabled check

--- HeroRotation/UI.lua
+++ HeroRotation/UI.lua
@@ -387,7 +387,7 @@
       local NameplateIconSize = Nameplate:GetHeight() / NameplateScaler;
       local HealthBar;
       if HR.GUISettings.General.NamePlateIconAnchor == "Life Bar" then
-        if _G.ElvUI and _G.ElvUI[1].NamePlates then
+        if _G.ElvUI and _G.ElvUI[1].charSettings.profile.nameplates.enable then
           HealthBar = Nameplate.unitFrame.Health;
           NameplateIconSize = HealthBar:GetWidth() / 3.5;
         elseif _G.ShestakUI and _G.ShestakUI[2].nameplate.enable then
