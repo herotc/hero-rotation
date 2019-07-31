a478152cb41855de079e0a71cc16a6354dcf046e
Cilraaz <cilraaz@gmail.com>
2019-07-31 16:50:23 -0400

    [DemonHunter] Repeat Performance Override Test

--- HeroRotation_DemonHunter/Overrides.lua
+++ HeroRotation_DemonHunter/Overrides.lua
@@ -1,24 +1,71 @@
 --- ============================ HEADER ============================
-  -- HeroLib
-  local HL      = HeroLib;
-  local Cache   = HeroCache;
-  local Unit    = HL.Unit;
-  local Player  = Unit.Player;
-  local Pet     = Unit.Pet;
-  local Target  = Unit.Target;
-  local Spell   = HL.Spell;
-  local Item    = HL.Item;
+-- HeroLib
+local HL      = HeroLib;
+local Cache   = HeroCache;
+local Unit    = HL.Unit;
+local Player  = Unit.Player;
+local Pet     = Unit.Pet;
+local Target  = Unit.Target;
+local Spell   = HL.Spell;
+local Item    = HL.Item;
 -- HeroRotation
-  local HR      = HeroRotation;
+local HR      = HeroRotation;
 -- Spells
-  local SpellHavoc       = Spell.DemonHunter.Havoc;
-  local SpellVengeance   = Spell.DemonHunter.Vengeance;
+local SpellHavoc              = Spell.DemonHunter.Havoc;
+local SpellVengeance          = Spell.DemonHunter.Vengeance;
+local RepeatPerformanceDebuff = Spell(304409);
 -- Lua
 
 --- ============================ CONTENT ============================
 -- Havoc, ID: 577
+local OldHavocIsCastableP
+OldHavocIsCastableP = HL.AddCoreOverride ("Spell.IsCastableP",
+function (self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  local BaseCheck = OldHavocIsCastableP(self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  if Player:InstanceInfo(8) == 2164 then
+    return BaseCheck and (not Player:DebuffP(RepeatPerformanceDebuff) or Player:DebuffP(RepeatPerformanceDebuff) and not Player:PrevGCDP(1, self))
+  else
+    return BaseCheck
+  end
+end
+, 577);
+
+local OldHavocIsReadyP
+OldHavocIsReadyP = HL.AddCoreOverride ("Spell.IsReadyP",
+function (self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  local BaseCheck = OldHavocIsReadyP(self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  if Player:InstanceInfo(8) == 2164 then
+    return BaseCheck and (not Player:DebuffP(RepeatPerformanceDebuff) or Player:DebuffP(RepeatPerformanceDebuff) and not Player:PrevGCDP(1, self))
+  else
+    return BaseCheck
+  end
+end
+, 577);
 
 -- Vengeance, ID: 581
+local OldVengIsCastable
+OldVengIsCastable = HL.AddCoreOverride ("Spell.IsCastable",
+function (self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  local BaseCheck = OldVengIsCastable(self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  if Player:InstanceInfo(8) == 2164 then
+    return BaseCheck and (not Player:DebuffP(RepeatPerformanceDebuff) or Player:DebuffP(RepeatPerformanceDebuff) and not Player:PrevGCDP(1, self))
+  else
+    return BaseCheck
+  end
+end
+, 581);
+
+local OldVengIsReady
+OldVengIsReady = HL.AddCoreOverride ("Spell.IsReady",
+function (self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  local BaseCheck = OldVengIsReady(self, Range, AoESpell, ThisUnit, BypassRecovery, Offset)
+  if Player:InstanceInfo(8) == 2164 then
+    return BaseCheck and (not Player:DebuffP(RepeatPerformanceDebuff) or Player:DebuffP(RepeatPerformanceDebuff) and not Player:PrevGCDP(1, self))
+  else
+    return BaseCheck
+  end
+end
+, 581);
 
 -- Example (Arcane Mage)
 -- HL.AddCoreOverride ("Spell.IsCastableP", 
