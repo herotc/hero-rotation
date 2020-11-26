101627015f93ef62d7aac08a7b977ee38c93f44b
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2020-11-26 10:35:33 +0100

    [M-Frost] Fix covenant spell setting

--- HeroRotation_Mage/Frost.lua
+++ HeroRotation_Mage/Frost.lua
@@ -187,15 +187,15 @@ local function Aoe ()
   end
   --radiant_spark
   if HR.CDsON() and S.RadiantSpark:IsCastable() then
-    if HR.Cast(S.RadiantSpark, nil, nil, not Target:IsSpellInRange(S.RadiantSpark)) then return "radiant_spark aoe 7"; end
+    if HR.Cast(S.RadiantSpark, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.RadiantSpark)) then return "radiant_spark aoe 7"; end
   end
   --mirrors_of_torment
   if HR.CDsON() and S.MirrorsofTorment:IsCastable() then
-    if HR.Cast(S.MirrorsofTorment, nil, nil, not Target:IsSpellInRange(S.MirrorsofTorment)) then return "mirrors_of_torment aoe 8"; end
+    if HR.Cast(S.MirrorsofTorment, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.MirrorsofTorment)) then return "mirrors_of_torment aoe 8"; end
   end
   --shifting_power
   if HR.CDsON() and S.ShiftingPower:IsCastable() then
-    if HR.Cast(S.ShiftingPower, nil, nil, not Target:IsSpellInRange(S.ShiftingPower)) then return "shifting_power aoe 9"; end
+    if HR.Cast(S.ShiftingPower, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.ShiftingPower)) then return "shifting_power aoe 9"; end
   end
   --frost_nova,if=runeforge.grisly_icicle.equipped&target.level<=level&debuff.frozen.down
   -- NYI frozen
@@ -267,7 +267,7 @@ local function Single ()
   end
   --radiant_spark,if=buff.freezing_winds.up&active_enemies=1
   if HR.CDsON() and S.RadiantSpark:IsCastable() and Player:BuffUp(S.FreezingWindsBuff) and EnemiesCount16ySplash == 1 then
-    if HR.Cast(S.RadiantSpark, nil, nil, not Target:IsSpellInRange(S.RadiantSpark)) then return "radiant_spark single 9"; end
+    if HR.Cast(S.RadiantSpark, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.RadiantSpark)) then return "radiant_spark single 9"; end
   end
   --ice_lance,if=buff.fingers_of_frost.react|debuff.frozen.remains>travel_time
   if S.IceLance:IsCastable() and (Player:BuffStack(S.FingersofFrostBuff) > 0 or Target:DebuffRemains(S.Freeze) > S.IceLance:TravelTime()) then
@@ -279,7 +279,7 @@ local function Single ()
   end
   --radiant_spark,if=(!runeforge.freezing_winds.equipped|active_enemies>=2)&buff.brain_freeze.react
   if HR.CDsON() and S.RadiantSpark:IsCastable() and (not FreezingWindsEquipped or EnemiesCount15yMelee >= 2) and Player:BuffUp(S.BrainFreezeBuff) then
-    if HR.Cast(S.RadiantSpark, nil, nil, not Target:IsSpellInRange(S.RadiantSpark)) then return "radiant_spark single 12"; end
+    if HR.Cast(S.RadiantSpark, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.RadiantSpark)) then return "radiant_spark single 12"; end
   end
   --mirrors_of_torment
   if HR.CDsON() and S.MirrorsofTorment:IsCastable() then
