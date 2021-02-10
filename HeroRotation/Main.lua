70270d6a1ab8d00d3188f7c1d1a338fc672622f9
Cilraaz <cilraaz@gmail.com>
2021-02-10 11:36:26 -0500

    fix(Hotkey): Don't resize CAO buttons with hotkey scaling

--- HeroRotation/Main.lua
+++ HeroRotation/Main.lua
@@ -281,7 +281,7 @@
           if CP_General then
             CreatePanelOption("Slider", CP_General, "General.ScaleUI", {0.5, 5, 0.1}, "UI Scale", "Scale of the Icons.", function(value) HR.MainFrame:ResizeUI(value); end);
             CreatePanelOption("Slider", CP_General, "General.ScaleButtons", {0.5, 5, 0.1}, "Buttons Scale", "Scale of the Buttons.", function(value) HR.MainFrame:ResizeButtons(value); end);
-            CreatePanelOption("Slider", CP_General, "General.ScaleHotkey", {0.5, 5, 0.1}, "Hotkey Scale", "Scale of the Hotkeys.", function(value) HR.MainFrame:ResizeButtons(value); end);
+            CreatePanelOption("Slider", CP_General, "General.ScaleHotkey", {0.5, 5, 0.1}, "Hotkey Scale", "Scale of the Hotkeys.");
             CreatePanelOption("Button", CP_General, "ButtonMove", "Lock/Unlock", "Enable the moving of the frames.", function() HR.MainFrame:ToggleLock(); end);
             CreatePanelOption("Button", CP_General, "ButtonReset", "Reset Buttons", "Resets the anchor of buttons.", function() HR.ToggleIconFrame:ResetAnchor(); end);
           end
