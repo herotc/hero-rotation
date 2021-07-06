3967af7144882cdc6cb6d8998b100d1bfbc4716a
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2021-07-06 18:58:10 +0200

    feat(repo): Update symlink

--- symlink.bat
+++ symlink.bat
@@ -1,6 +1,5 @@
-REM Modify the two vars so it match you own setup. Make sure you have it surrounded by double quotes
+REM Modify the WoWRep var so it match you own setup. Make sure you have it surrounded by double quotes
 REM WoWRep  : World of warcraft main directory
-REM GHRep   : Where your github projects are stored (by default in Documents/GitHub)
 set WoWRep="G:\World of Warcraft"
 set CWD=%cd%
 
@@ -8,15 +7,15 @@ REM Don't touch anything bellow this if you aren't experienced
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation" %CWD%"\hero-rotation\HeroRotation"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_DeathKnight" %CWD%"\hero-rotation\HeroRotation_DeathKnight"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_DemonHunter" %CWD%"\hero-rotation\HeroRotation_DemonHunter"
-REM mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Druid" %CWD%"\hero-rotation\HeroRotation_Druid"
+mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Druid" %CWD%"\hero-rotation\HeroRotation_Druid"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Hunter" %CWD%"\hero-rotation\HeroRotation_Hunter"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Mage" %CWD%"\hero-rotation\HeroRotation_Mage"
-REM mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Monk" %CWD%"\hero-rotation\HeroRotation_Monk"
-REM mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Paladin" %CWD%"\hero-rotation\HeroRotation_Paladin"
+mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Monk" %CWD%"\hero-rotation\HeroRotation_Monk"
+mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Paladin" %CWD%"\hero-rotation\HeroRotation_Paladin"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Priest" %CWD%"\hero-rotation\HeroRotation_Priest"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Rogue" %CWD%"\hero-rotation\HeroRotation_Rogue"
-REM mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Shaman" %CWD%"\hero-rotation\HeroRotation_Shaman"
-REM mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Warrior" %CWD%"\hero-rotation\HeroRotation_Warrior"
+mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Shaman" %CWD%"\hero-rotation\HeroRotation_Shaman"
+mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Warrior" %CWD%"\hero-rotation\HeroRotation_Warrior"
 mklink /J %WoWRep%"\_retail_\Interface\AddOns\HeroRotation_Warlock" %CWD%"\hero-rotation\HeroRotation_Warlock"
 
 pause
--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# Modify the two vars so it match you own setup. Make sure you have it surrounded by double quotes
+# Modify the WoWRep var so it match you own setup. Make sure you have it surrounded by double quotes
 # WoWRep  : World of warcraft main directory
-# GHRep   : Where your github projects are stored (by default in Documents/GitHub)
 WoWRep="/Applications/World of Warcraft"
 CWD=$(pwd)
 
@@ -10,13 +9,13 @@ CWD=$(pwd)
 ln -s "$CWD/../hero-rotation/HeroRotation" "$WoWRep/_retail_/Interface/AddOns/HeroRotation"
 ln -s "$CWD/../hero-rotation/HeroRotation_DeathKnight" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_DeathKnight"
 ln -s "$CWD/../hero-rotation/HeroRotation_DemonHunter" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_DemonHunter"
-# ln -s "$CWD/../hero-rotation/HeroRotation_Druid" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Druid"
+ln -s "$CWD/../hero-rotation/HeroRotation_Druid" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Druid"
 ln -s "$CWD/../hero-rotation/HeroRotation_Hunter" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Hunter"
 ln -s "$CWD/../hero-rotation/HeroRotation_Mage" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Mage"
-# ln -s "$CWD/../hero-rotation/HeroRotation_Monk" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Monk"
-# ln -s "$CWD/../hero-rotation/HeroRotation_Paladin" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Paladin"
+ln -s "$CWD/../hero-rotation/HeroRotation_Monk" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Monk"
+ln -s "$CWD/../hero-rotation/HeroRotation_Paladin" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Paladin"
 ln -s "$CWD/../hero-rotation/HeroRotation_Priest" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Priest"
 ln -s "$CWD/../hero-rotation/HeroRotation_Rogue" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Rogue"
-# ln -s "$CWD/../hero-rotation/HeroRotation_Shaman" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Shaman"
-# ln -s "$CWD/../hero-rotation/HeroRotation_Warrior" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Warrior"
+ln -s "$CWD/../hero-rotation/HeroRotation_Shaman" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Shaman"
+ln -s "$CWD/../hero-rotation/HeroRotation_Warrior" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Warrior"
 ln -s "$CWD/../hero-rotation/HeroRotation_Warlock" "$WoWRep/_retail_/Interface/AddOns/HeroRotation_Warlock"
