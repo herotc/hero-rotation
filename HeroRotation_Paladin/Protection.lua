d286134888239928f0b493e12c37e48f825bb82a
Matt Redmond <mttrdmnd@gmail.com>
2022-01-05 15:57:47 -0800

    [prot pal] A few changes to better position icons + some logic updates.

--- HeroRotation_Paladin/Protection.lua
+++ HeroRotation_Paladin/Protection.lua
@@ -54,6 +54,10 @@ local function EvaluateCycleJudgment200(TargetUnit)
   return TargetUnit:DebuffRefreshable(S.JudgmentDebuff)
 end
 
+local function EvaluateCycleHammerofWrath(TargetUnit)
+  return TargetUnit:HealthPercentage() < 20 or Player:BuffUp(S.AvengingWrathBuff)
+end
+
 local function Precombat()
   -- flask
   -- food
@@ -86,17 +90,21 @@ local function Precombat()
 end
 
 local function Defensives()
+  if Player:HealthPercentage() <= 10 and S.LayonHands:IsCastable() then
+    if HR.CastSuggested(S.Layonhands) then return "LOH"; end
+  end
   if S.GuardianofAncientKings:IsCastable() and (Player:HealthPercentage() <= Settings.Protection.GoAKHP and Player:BuffDown(S.ArdentDefenderBuff)) then
-    if HR.Cast(S.GuardianofAncientKings) then return "guardian_of_ancient_kings defensive"; end
+    if HR.CastSuggested(S.GuardianofAncientKings) then return "guardian_of_ancient_kings defensive"; end
   end
   if S.ArdentDefender:IsCastable() and (Player:HealthPercentage() <= Settings.Protection.ArdentDefenderHP and Player:BuffDown(S.GuardianofAncientKingsBuff)) then
-    if HR.Cast(S.ArdentDefender) then return "ardent_defender defensive"; end
+    if HR.CastSuggested(S.ArdentDefender) then return "ardent_defender defensive"; end
   end
+  -- todo: logic right here to check if you have enough SOTR buff up - you might have to choose between dropping buff and healing self
   if S.WordofGlory:IsReady() and (Player:HealthPercentage() <= Settings.Protection.WordofGloryHP and not Player:HealingAbsorbed()) then
-    if HR.Cast(S.WordofGlory, Settings.Protection.GCDasOffGCD.WordOfGlory) then return "word_of_glory defensive"; end
+    if HR.Cast(S.WordofGlory) then return "word_of_glory defensive"; end
   end
   if S.ShieldoftheRighteous:IsReady() and (Player:BuffRefreshable(S.ShieldoftheRighteousBuff) and (ActiveMitigationNeeded or Player:HealthPercentage() <= Settings.Protection.ShieldoftheRighteousHP)) then
-    if HR.Cast(S.ShieldoftheRighteous, Settings.Protection.OffGCDasOffGCD.ShieldOfTheRighteous) then return "shield_of_the_righteous defensive"; end
+    if HR.CastRightSuggested(S.ShieldoftheRighteous) then return "shield_of_the_righteous defensive"; end
   end
 end
 
@@ -116,6 +124,14 @@ local function Cooldowns()
   -- holy_avenger,if=buff.avenging_wrath.up|cooldown.avenging_wrath.remains>60
   if S.HolyAvenger:IsCastable() and (Player:BuffUp(S.AvengingWrathBuff) or S.AvengingWrath:CooldownRemains() > 60) then
     if HR.Cast(S.HolyAvenger) then return "holy_avenger 38"; end
+  end
+  -- ashen_hallow
+  if S.AshenHallow:IsReady() then
+    if HR.Cast(S.AshenHallow, nil, Settings.Commons.CovenantDisplayStyle) then return "ashen_hallow 84"; end
+  end
+  -- divine_toll
+  if S.DivineToll:IsReady() and Player:HolyPower() <= 1 then
+    if HR.Cast(S.DivineToll, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.DivineToll)) then return "divine_toll 80"; end
   end
   -- potion,if=buff.avenging_wrath.up
   if I.PotionofUnbridledFury:IsReady() and Settings.Commons.UsePotions and (Player:BuffUp(S.AvengingWrathBuff)) then
@@ -137,12 +153,19 @@ end
 local function Standard()
   -- shield_of_the_righteous,if=debuff.judgment.up&(debuff.vengeful_shock.up|!conduit.vengeful_shock.enabled)
   if S.ShieldoftheRighteous:IsReady() and (Target:DebuffUp(S.JudgmentDebuff) and (Target:DebuffUp(S.VengefulShockDebuff) or not S.VengefulShock:ConduitEnabled())) then
-    if HR.Cast(S.ShieldoftheRighteous, Settings.Protection.OffGCDasOffGCD.ShieldOfTheRighteous, nil, not Target:IsSpellInRange(S.ShieldoftheRighteous)) then return "shield_of_the_righteous 62"; end
+    if HR.CastRightSuggested(S.ShieldoftheRighteous) then return "shield_of_the_righteous 62"; end
   end
+
   -- shield_of_the_righteous,if=holy_power=5|buff.holy_avenger.up|holy_power=4&talent.sanctified_wrath.enabled&buff.avenging_wrath.up
   if S.ShieldoftheRighteous:IsReady() and (Player:HolyPower() == 5 or Player:BuffUp(S.HolyAvengerBuff) or Player:HolyPower() == 4 and S.SanctifiedWrath:IsAvailable() and Player:BuffUp(S.AvengingWrathBuff)) then
-    if HR.Cast(S.ShieldoftheRighteous, Settings.Protection.OffGCDasOffGCD.ShieldOfTheRighteous, nil, not Target:IsSpellInRange(S.ShieldoftheRighteous)) then return "shield_of_the_righteous 64"; end
+    if HR.CastRightSuggested(S.ShieldoftheRighteous) then return "shield_of_the_righteous 64"; end
   end
+
+  -- NOTE(MRDMND) - added an AOE prio on this over judgment
+  if S.AvengersShield:IsCastable() and #Player:GetEnemiesInMeleeRange(8) >= 3 then
+    if HR.Cast(S.AvengersShield, nil, nil, not Target:IsSpellInRange(S.AvengersShield)) then return "avengers_shield 68"; end
+  end
+
   -- judgment,target_if=min:debuff.judgment.remains,if=charges=2|!talent.crusaders_judgment.enabled
   if S.Judgment:IsReady() and (S.Judgment:Charges() == 2 or not S.CrusadersJudgment:IsAvailable()) then
     if Everyone.CastCycle(S.Judgment, Enemies30y, EvaluateCycleJudgment200, not Target:IsSpellInRange(S.Judgment)) then return "judgment 66"; end
@@ -154,7 +177,7 @@ local function Standard()
   -- hammer_of_wrath
   -- Note: Added IsUsable check. IsReady checks IsCastable and IsUsableP, which always returns true when not on CD
   if S.HammerofWrath:IsReady() and S.HammerofWrath:IsUsable() then
-    if HR.Cast(S.HammerofWrath, nil, nil, not Target:IsSpellInRange(S.HammerofWrath)) then return "hammer_of_wrath 70"; end
+    if Everyone.CastCycle(S.HammerofWrath, Enemies30y, EvaluateCycleHammerofWrath, not Target:IsSpellInRange(S.HammerofWrath)) then return "hammer of wrath"; end
   end
   -- avengers_shield
   if S.AvengersShield:IsCastable() then
@@ -164,33 +187,26 @@ local function Standard()
   if S.Judgment:IsReady() then
     if Everyone.CastCycle(S.Judgment, Enemies30y, EvaluateCycleJudgment200, not Target:IsSpellInRange(S.Judgment)) then return "judgment 74"; end
   end
+  -- consecration,if=!consecration.up k
+  if S.Consecration:IsCastable() and (Player:BuffDown(S.ConsecrationBuff)) then
+    if HR.Cast(S.Consecration) then return "consecration 78"; end
+  end
   -- vanquishers_hammer
   if S.VanquishersHammer:IsReady() then
     if HR.Cast(S.VanquishersHammer, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.VanquishersHammer)) then return "vanquishers_hammer 76"; end
   end
-  -- consecration,if=!consecration.up
-  if S.Consecration:IsCastable() and (Player:BuffDown(S.ConsecrationBuff)) then
-    if HR.Cast(S.Consecration) then return "consecration 78"; end
-  end
-  -- divine_toll
-  if S.DivineToll:IsReady() then
-    if HR.Cast(S.DivineToll, nil, Settings.Commons.CovenantDisplayStyle, not Target:IsSpellInRange(S.DivineToll)) then return "divine_toll 80"; end
-  end
-  -- blessed_hammer,strikes=2.4,if=charges=3
+    -- blessed_hammer,strikes=2.4,if=charges=3
   if S.BlessedHammer:IsCastable() and (S.BlessedHammer:Charges() == 3) then
     if HR.Cast(S.BlessedHammer, nil, nil, not Target:IsInMeleeRange(5)) then return "blessed_hammer 82"; end
   end
-  -- ashen_hallow
-  if S.AshenHallow:IsReady() then
-    if HR.Cast(S.AshenHallow, nil, Settings.Commons.CovenantDisplayStyle) then return "ashen_hallow 84"; end
-  end
+  
   -- hammer_of_the_righteous,if=charges=2
   if S.HammeroftheRighteous:IsCastable() and (S.HammeroftheRighteous:Charges() == 2) then
     if HR.Cast(S.HammeroftheRighteous, nil, nil, not Target:IsInMeleeRange(5)) then return "hammer_of_the_righteous 86"; end
   end
   -- word_of_glory,if=buff.vanquishers_hammer.up
   if S.WordofGlory:IsReady() and (Player:BuffUp(S.VanquishersHammerBuff)) then
-    if HR.Cast(S.WordofGlory, Settings.Protection.GCDasOffGCD.WordOfGlory) then return "word_of_glory 88"; end
+    if HR.Cast(S.WordofGlory) then return "word_of_glory 88"; end
   end
   -- blessed_hammer,strikes=2.4
   if S.BlessedHammer:IsCastable() then
@@ -206,16 +222,18 @@ local function Standard()
   end
   -- arcane_torrent
   if S.ArcaneTorrent:IsCastable() then
-    if HR.Cast(S.ArcaneTorrent, Settings.Commons.OffGCDasOffGCD.Racials, nil, not Target:IsInRange(8)) then return "arcane_torrent 96"; end
+    if HR.Cast(S.ArcaneTorrent) then return "arcane_torrent 96"; end
   end
   -- consecration
   if S.Consecration:IsCastable() then
     if HR.Cast(S.Consecration) then return "consecration 98"; end
   end
   -- word_of_glory,if=buff.shining_light_free.up&!covenant.necrolord
+  -- TODO: this should not fire if everyone in your party is full HP, but we DO want to heal teammates if we're topped at this point
   if S.WordofGlory:IsReady() and (Player:BuffUp(S.ShiningLightFreeBuff) and not S.VanquishersHammer:IsAvailable()) then
-    if HR.Cast(S.WordofGlory, Settings.Protection.GCDasOffGCD.WordOfGlory) then return "word_of_glory 100"; end
+    if HR.Cast(S.WordofGlory) then return "word_of_glory 100"; end
   end
+  
 end
 
 -- APL Main
