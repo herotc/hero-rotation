c0a00f14b4c2dc5317a4754ac6c6d02110881629
Cilraaz <cilraaz@gmail.com>
2022-02-18 12:09:38 -0500

    feat(Hunter): Comment out Init WIP messages
    - Profiles fully updated for 9.1.5 and have some 9.2 updates already

--- HeroRotation_Hunter/Marksmanship.lua
+++ HeroRotation_Hunter/Marksmanship.lua
@@ -530,7 +530,7 @@ local function APL()
 end
 
 local function Init()
-  HR.Print("Marksmanship Hunter rotation is currently a work in progress, but has been updated for patch 9.1.")
+  --HR.Print("Marksmanship Hunter rotation is currently a work in progress, but has been updated for patch 9.1.5.")
 end
 
 HR.SetAPL(254, APL, Init)
