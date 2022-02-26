d2afe4bfbecfefcbbdaae823cf3e4c10b0a23b75
Cilraaz <cilraaz@gmail.com>
2022-02-26 10:56:07 -0500

    feat(DemonHunter): Use Spell/Items commons with MergeTableByKey

--- HeroRotation_DemonHunter/DemonHunter.lua
+++ HeroRotation_DemonHunter/DemonHunter.lua
@@ -14,6 +14,7 @@ local Pet        = Unit.Pet
 local Spell      = HL.Spell
 local MultiSpell = HL.MultiSpell
 local Item       = HL.Item
+local MergeTableByKey = HL.Utils.MergeTableByKey
 -- HeroRotation
 local HR         = HeroRotation
 
@@ -21,45 +22,16 @@ local HR         = HeroRotation
 
 -- Spell
 if not Spell.DemonHunter then Spell.DemonHunter = {} end
-Spell.DemonHunter.Vengeance = {
+Spell.DemonHunter.Commons = {
+  -- Racials
+  ArcaneTorrent                         = Spell(50613),
   -- Abilities
-  FelDevastation                        = Spell(212084),
-  Frailty                               = Spell(247456),
   ImmolationAura                        = Spell(258920),
   ImmolationAuraBuff                    = Spell(258920),
-  InfernalStrike                        = Spell(189110),
-  Shear                                 = Spell(203782),
-  SigilofFlame                          = MultiSpell(204596, 204513),
-  SigilofFlameDebuff                    = Spell(204598),
-  SoulCleave                            = Spell(228477),
-  SoulFragments                         = Spell(203981),
-  ThrowGlaive                           = Spell(204157),
-
-  -- Defensive
-  DemonSpikes                           = Spell(203720),
-  DemonSpikesBuff                       = Spell(203819),
-  FieryBrand                            = Spell(204021),
-  FieryBrandDebuff                      = Spell(207771),
-  Torment                               = Spell(185245),
-
   -- Talents
-  AgonizingFlames                       = Spell(207548),
-  BulkExtraction                        = Spell(320341),
-  BurningAlive                          = Spell(207739),
-  CharredFlesh                          = Spell(336639),
-  ConcentratedSigils                    = Spell(207666),
-  Demonic                               = Spell(321453),
   Felblade                              = Spell(232893),
-  Fracture                              = Spell(263642),
-  SoulBarrier                           = Spell(263648),
-  SpiritBomb                            = Spell(247454),
-  SpiritBombDebuff                      = Spell(247456),
-
   -- Utility
   Disrupt                               = Spell(183752),
-  Metamorphosis                         = Spell(187827),
-  MetamorphosisBuff                     = Spell(187827),
-
   -- Covenant Abilities
   DoorofShadows                         = Spell(300728),
   ElysianDecree                         = Spell(306830),
@@ -69,21 +41,52 @@ Spell.DemonHunter.Vengeance = {
   Soulshape                             = Spell(310143),
   SummonSteward                         = Spell(324739),
   TheHunt                               = Spell(323639),
-
   -- Legendary Effects
+  ChaosTheoryBuff                       = Spell(337567),
   FelBombardmentBuff                    = Spell(337849),
-
+  BurningWoundDebuff                    = Spell(346278),
   -- Soulbind/Conduit Effects
   EnduringGloom                         = Spell(319978),
-
+  ExposedWoundDebuff                    = Spell(339229), -- Triggered by Serrated Glaive
+  SerratedGlaive                        = Spell(339230),
   -- Other
   Pool                                  = Spell(999910)
 }
 
-Spell.DemonHunter.Havoc = {
-  -- Racials
-  ArcaneTorrent                         = Spell(50613),
+Spell.DemonHunter.Vengeance = MergeTableByKey(Spell.DemonHunter.Commons, {
+  -- Abilities
+  FelDevastation                        = Spell(212084),
+  Frailty                               = Spell(247456),
+  InfernalStrike                        = Spell(189110),
+  Shear                                 = Spell(203782),
+  SigilofFlame                          = MultiSpell(204596, 204513),
+  SigilofFlameDebuff                    = Spell(204598),
+  SoulCleave                            = Spell(228477),
+  SoulFragments                         = Spell(203981),
+  ThrowGlaive                           = Spell(204157),
+  -- Defensive
+  DemonSpikes                           = Spell(203720),
+  DemonSpikesBuff                       = Spell(203819),
+  FieryBrand                            = Spell(204021),
+  FieryBrandDebuff                      = Spell(207771),
+  Torment                               = Spell(185245),
+  -- Talents
+  AgonizingFlames                       = Spell(207548),
+  BulkExtraction                        = Spell(320341),
+  BurningAlive                          = Spell(207739),
+  CharredFlesh                          = Spell(336639),
+  ConcentratedSigils                    = Spell(207666),
+  Demonic                               = Spell(321453),
+  Fracture                              = Spell(263642),
+  SoulBarrier                           = Spell(263648),
+  SpiritBomb                            = Spell(247454),
+  SpiritBombDebuff                      = Spell(247456),
+  -- Utility
+  Metamorphosis                         = Spell(187827),
+  MetamorphosisBuff                     = Spell(187827)
+})
 
+Spell.DemonHunter.Havoc = MergeTableByKey(Spell.DemonHunter.Commons, {
   -- Abilities
   Annihilation                          = Spell(201427),
   BladeDance                            = Spell(188499),
@@ -91,18 +94,14 @@ Spell.DemonHunter.Havoc = {
   ChaosStrike                           = Spell(162794),
   DeathSweep                            = Spell(210152),
   DemonsBite                            = Spell(162243),
-  Disrupt                               = Spell(183752),
   EyeBeam                               = Spell(198013),
   FelRush                               = Spell(195072),
   FuriousGazeBuff                       = Spell(343312),
-  ImmolationAura                        = Spell(258920),
-  ImmolationAuraBuff                    = Spell(258920),
   Metamorphosis                         = Spell(191427),
   MetamorphosisBuff                     = Spell(162264),
   ThrowGlaive                           = Spell(185123),
   VengefulRetreat                       = Spell(198793),
   Blur                                  = Spell(198589),
-
   -- Talents
   BlindFury                             = Spell(203550),
   CycleofHatred                         = Spell(258887),
@@ -112,7 +111,6 @@ Spell.DemonHunter.Havoc = {
   EssenceBreakDebuff                    = Spell(320338),
   FelBarrage                            = Spell(258925),
   FelEruption                           = Spell(211881),
-  Felblade                              = Spell(232893),
   FirstBlood                            = Spell(206416),
   GlaiveTempest                         = Spell(342817),
   Momentum                              = Spell(206476),
@@ -120,35 +118,19 @@ Spell.DemonHunter.Havoc = {
   PreparedBuff                          = Spell(203650), -- Procs from Vengeful Retreat with Momentum
   TrailofRuin                           = Spell(258881),
   UnboundChaos                          = Spell(347461),
-  UnboundChaosBuff                      = Spell(347462),
-
-  -- Covenant Abilities
-  ElysianDecree                         = Spell(306830),
-  SinfulBrand                           = Spell(317009),
-  SinfulBrandDebuff                     = Spell(317009),
-  TheHunt                               = Spell(323639),
-
-  -- Legendary Effects
-  ChaosTheoryBuff                       = Spell(337567),
-  FelBombardmentBuff                    = Spell(337849),
-  BurningWoundDebuff                    = Spell(346278),
-
-  -- Conduits
-  ExposedWoundDebuff                    = Spell(339229), -- Triggered by Serrated Glaive
-  SerratedGlaive                        = Spell(339230),
-
-  -- Other
-  Pool                                  = Spell(999910)
-}
+  UnboundChaosBuff                      = Spell(347462)
+})
 
 -- Items
 if not Item.DemonHunter then Item.DemonHunter = {} end
-Item.DemonHunter.Vengeance = {
+Item.DemonHunter.Commons = {
   PotionofPhantomFire              = Item(171349),
   PulsatingStoneheart              = Item(178825, {13, 14}),
   DarkmoonDeckIndomitable          = Item(173096, {13, 14}),
 }
 
-Item.DemonHunter.Havoc = {
-  PotionofPhantomFire              = Item(171349),
-}
+Item.DemonHunter.Vengeance = MergeTableByKey(Item.DemonHunter.Commons, {
+})
+
+Item.DemonHunter.Havoc = MergeTableByKey(Item.DemonHunter.Commons, {
+})
