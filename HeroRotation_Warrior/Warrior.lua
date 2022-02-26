aa3b21954f31469f5cb9176490457d7ea83b9d94
Cilraaz <cilraaz@gmail.com>
2022-02-26 17:04:55 -0500

    feat(Warrior): Use Spell/Item commons via MergeTableByKey

--- HeroRotation_Warrior/Warrior.lua
+++ HeroRotation_Warrior/Warrior.lua
@@ -14,6 +14,7 @@ local Pet        = Unit.Pet
 local Spell      = HL.Spell
 local MultiSpell = HL.MultiSpell
 local Item       = HL.Item
+local MergeTableByKey = HL.Utils.MergeTableByKey
 -- HeroRotation
 local HR         = HeroRotation
 
@@ -21,8 +22,7 @@ local HR         = HeroRotation
 
 -- Spell
 if not Spell.Warrior then Spell.Warrior = {} end
-
-Spell.Warrior.Fury = {
+Spell.Warrior.Commons = {
   -- Racials
   AncestralCall                         = Spell(274738),
   ArcaneTorrent                         = Spell(50613),
@@ -31,47 +31,22 @@ Spell.Warrior.Fury = {
   BloodFury                             = Spell(20572),
   Fireblood                             = Spell(265221),
   LightsJudgment                        = Spell(255647),
-
   -- Abilities
   BattleShout                           = Spell(6673),
   BattleShoutBuff                       = Spell(6673),
-  Bloodbath                             = Spell(335096),
-  Bloodthirst                           = Spell(23881),
   Charge                                = Spell(100),
-  CrushingBlow                          = Spell(335097),
-  EnrageBuff                            = Spell(184362),
-  Execute                               = MultiSpell(5308, 280735),
-  FujiedasFuryBuff                      = Spell(207775),
   IntimidatingShout                     = Spell(5246),
   HeroicLeap                            = Spell(6544),
-  MeatCleaverBuff                       = Spell(85739),
   Pummel                                = Spell(6552),
-  RagingBlow                            = Spell(85288),
-  Rampage                               = Spell(184367),
-  RecklessnessBuff                      = Spell(1719),
-  Recklessness                          = Spell(1719),
   VictoryRush                           = Spell(34428),
-  Whirlwind                             = Spell(190411),
-
   -- Talents
   AngerManagement                       = Spell(152278),
-  Bladestorm                            = Spell(46924),
-  Cruelty                               = Spell(335070),
+  Avatar                                = Spell(107574),
+  AvatarBuff                            = Spell(107574),
   DragonRoar                            = Spell(118000),
-  Frenzy                                = Spell(335077),
-  FrenzyBuff                            = Spell(335077),
-  FrothingBerserker                     = Spell(215571),
   ImpendingVictory                      = Spell(202168),
-  Massacre                              = Spell(206315),
-  Onslaught                             = Spell(315720),
-  RecklessAbandon                       = Spell(202751),
-  Siegebreaker                          = Spell(280772),
-  SiegebreakerDebuff                    = Spell(280773),
   StormBolt                             = Spell(107570),
-  SuddenDeath                           = Spell(280721),
-  SuddenDeathBuff                       = Spell(280776),
-
-  -- Covenant Abilities
+  -- Covenant Abilities (Shadowlands)
   AncientAftershock                     = Spell(325886),
   Condemn                               = MultiSpell(330325, 317485),
   CondemnDebuff                         = Spell(317491),
@@ -79,55 +54,60 @@ Spell.Warrior.Fury = {
   ConquerorsFrenzyBuff                  = Spell(343672),
   SpearofBastion                        = Spell(307865),
   SpearofBastionBuff                    = Spell(307871),
-
-  -- Legendary Effects
-  WilloftheBerserkerBuff                = Spell(335594),
-
-  -- Conduits
+  -- Conduits (Shadowlands)
   MercilessBonegrinder                  = Spell(335260),
   MercilessBonegrinderBuff              = Spell(346574),
-  ViciousContempt                       = Spell(337302),
-
-  -- Item Buffs/Debuffs
-
   -- Pool
   Pool                                  = Spell(999910),
 }
 
-Spell.Warrior.Arms = {
-  -- Racials
-  AncestralCall                         = Spell(274738),
-  ArcaneTorrent                         = Spell(50613),
-  BagofTricks                           = Spell(312411),
-  Berserking                            = Spell(26297),
-  BloodFury                             = Spell(20572),
-  Fireblood                             = Spell(265221),
-  LightsJudgment                        = Spell(255647),
-
+Spell.Warrior.Fury = MergeTableByKey(Spell.Warrior.Commons, {
   -- Abilities
-  BattleShout                           = Spell(6673),
-  BattleShoutBuff                       = Spell(6673),
+  Bloodbath                             = Spell(335096),
+  Bloodthirst                           = Spell(23881),
+  CrushingBlow                          = Spell(335097),
+  EnrageBuff                            = Spell(184362),
+  Execute                               = MultiSpell(5308, 280735),
+  MeatCleaverBuff                       = Spell(85739),
+  RagingBlow                            = Spell(85288),
+  Rampage                               = Spell(184367),
+  Recklessness                          = Spell(1719),
+  RecklessnessBuff                      = Spell(1719),
+  Whirlwind                             = Spell(190411),
+  -- Talents
+  Bladestorm                            = Spell(46924),
+  Cruelty                               = Spell(335070),
+  Frenzy                                = Spell(335077),
+  FrenzyBuff                            = Spell(335077),
+  FrothingBerserker                     = Spell(215571),
+  Massacre                              = Spell(206315),
+  Onslaught                             = Spell(315720),
+  RecklessAbandon                       = Spell(202751),
+  Siegebreaker                          = Spell(280772),
+  SiegebreakerDebuff                    = Spell(280773),
+  SuddenDeath                           = Spell(280721),
+  SuddenDeathBuff                       = Spell(280776),
+  -- Conduits (Shadowlands)
+  ViciousContempt                       = Spell(337302),
+  -- Legendary Effects (Shadowlands)
+  WilloftheBerserkerBuff                = Spell(335594),
+})
+
+Spell.Warrior.Arms = MergeTableByKey(Spell.Warrior.Commons, {
+  -- Abilities
   Bladestorm                            = Spell(227847),
-  Charge                                = Spell(100),
   ColossusSmash                         = Spell(167105),
   ColossusSmashDebuff                   = Spell(208086),
   DeepWoundsDebuff                      = Spell(262115),
   Execute                               = MultiSpell(163201, 281000),
-  HeroicLeap                            = Spell(6544),
-  IntimidatingShout                     = Spell(5246),
   MortalStrike                          = Spell(12294),
   Overpower                             = Spell(7384),
   OverpowerBuff                         = Spell(7384),
-  Pummel                                = Spell(6552),
   Slam                                  = Spell(1464),
   SweepingStrikes                       = Spell(260708),
   SweepingStrikesBuff                   = Spell(260708),
-  VictoryRush                           = Spell(34428),
   Whirlwind                             = Spell(1680),
-
   -- Talents
-  AngerManagement                       = Spell(152278),
-  Avatar                                = Spell(107574),
   Cleave                                = Spell(845),
   CollateralDamage                      = Spell(334779),
   DeadlyCalm                            = Spell(262228),
@@ -135,124 +115,67 @@ Spell.Warrior.Arms = {
   Doubletime                            = Spell(103827),
   Dreadnaught                           = Spell(262150),
   FervorofBattle                        = Spell(202316),
-  ImpendingVictory                      = Spell(202168),
-  Inforthekill                          = Spell(248621),
+  InfortheKill                          = Spell(248621),
   Massacre                              = Spell(281001),
   Ravager                               = Spell(152277),
   Rend                                  = Spell(772),
   RendDebuff                            = Spell(772),
   Skullsplitter                         = Spell(260643),
-  StormBolt                             = Spell(107570),
   SuddenDeathBuff                       = Spell(52437),
   Warbreaker                            = Spell(262161),
   WarMachineBuff                        = Spell(262231),
-
-  -- Covenant Abilities
-  AncientAftershock                     = Spell(325886),
-  Condemn                               = MultiSpell(330334, 317349),
-  CondemnDebuff                         = Spell(317491),
-  ConquerorsBanner                      = Spell(324143),
-  ConquerorsFrenzyBuff                  = Spell(343672),
-  SpearofBastion                        = Spell(307865),
-  SpearofBastionBuff                    = Spell(307871),
-
-  -- Legendary Effects
-
-  -- Conduits
+  -- Conduits (Shadowlands)
   AshenJuggernaut                       = Spell(335232),
   AshenJuggernautBuff                   = Spell(335234),
   BattlelordBuff                        = Spell(346369),
   ExploiterDebuff                       = Spell(335452),
+})
 
-  -- Item Buffs/Debuffs
-
-  -- Pool
-  Pool                                  = Spell(999910),
-}
-
-Spell.Warrior.Protection = {
-  -- Racials
-  AncestralCall                         = Spell(274738),
-  ArcaneTorrent                         = Spell(50613),
-  BagofTricks                           = Spell(312411),
-  Berserking                            = Spell(26297),
-  BloodFury                             = Spell(20572),
-  Fireblood                             = Spell(265221),
-  LightsJudgment                        = Spell(255647),
-
+Spell.Warrior.Protection = MergeTableByKey(Spell.Warrior.Commons, {
   -- Abilities
-  Avatar                                = Spell(107574),
-  AvatarBuff                            = Spell(107574),
-  Charge                                = Spell(100),
   DemoralizingShout                     = Spell(1160),
   Devastate                             = Spell(20243),
   Execute                               = Spell(163201),
   IgnorePain                            = Spell(190456),
-  IntimidatingShout                     = Spell(5246),
   LastStand                             = Spell(12975),
   LastStandBuff                         = Spell(12975),
-  Pummel                                = Spell(6552),
   Revenge                               = Spell(6572),
   RevengeBuff                           = Spell(5302),
   ShieldBlock                           = Spell(2565),
   ShieldBlockBuff                       = Spell(132404),
   ShieldSlam                            = Spell(23922),
   ThunderClap                           = Spell(6343),
-  VictoryRush                           = Spell(34428),
-
   -- Talents
   BoomingVoice                          = Spell(202743),
-  DragonRoar                            = Spell(118000),
-  ImpendingVictory                      = Spell(202168),
   Ravager                               = Spell(228920),
-  StormBolt                             = Spell(107570),
   UnstoppableForce                      = Spell(275336),
-
-  -- Covenant Abilities
-  AncientAftershock                     = Spell(325886),
-  Condemn                               = Spell(317349),
-  CondemnDebuff                         = Spell(317491),
-  ConquerorsBanner                      = Spell(324143),
-  ConquerorsFrenzyBuff                  = Spell(343672),
-  SpearofBastion                        = Spell(307865),
-  SpearofBastionBuff                    = Spell(307871),
-
-  -- Legendary Effects
-
-  -- Conduits
-
-  -- Item Buffs/Debuffs
-
-  -- Pool
-  Pool                                  = Spell(999910),
-}
+})
 
 -- Items
 if not Item.Warrior then Item.Warrior = {} end
-Item.Warrior.Fury = {
-  -- Potion
-  PotionofSpectralStrength         = Item(171275),
+Item.Warrior.Commons = {
+  -- Potions
+  PotionofPhantomFire                   = Item(171349),
+  PotionofSpectralStrength              = Item(171275),
   -- Trinkets
-  DDVoracity                       = Item(173087, {13, 14}),
-  FlameofBattle                    = Item(181501, {13, 14}),
-  GrimCodex                        = Item(178811, {13, 14}),
-  InscrutableQuantumDevice         = Item(179350, {13, 14}),
-  InstructorsDivineBell            = Item(184842, {13, 14}),
-  MacabreSheetMusic                = Item(184024, {13, 14}),
-  OverwhelmingPowerCrystal         = Item(179342, {13, 14}),
-  WakenersFrond                    = Item(181457, {13, 14}),
+  DDVoracity                            = Item(173087, {13, 14}),
+  FlameofBattle                         = Item(181501, {13, 14}),
+  GrimCodex                             = Item(178811, {13, 14}),
+  InscrutableQuantumDevice              = Item(179350, {13, 14}),
+  InstructorsDivineBell                 = Item(184842, {13, 14}),
+  MacabreSheetMusic                     = Item(184024, {13, 14}),
+  OverwhelmingPowerCrystal              = Item(179342, {13, 14}),
+  WakenersFrond                         = Item(181457, {13, 14}),
   -- Gladiator's Badges
-  SinfulGladiatorsBadge            = Item(175921, {13, 14}),
-  UnchainedGladiatorsBadge         = Item(185197, {13, 14}),
+  SinfulGladiatorsBadge                 = Item(175921, {13, 14}),
+  UnchainedGladiatorsBadge              = Item(185197, {13, 14}),
 }
 
-Item.Warrior.Arms = {
-  PotionofSpectralStrength         = Item(171275),
-  GrimCodex                        = Item(178811, {13, 14}),
-  DDVoracity                       = Item(173087, {13, 14}),
-}
+Item.Warrior.Fury = MergeTableByKey(Item.Warrior.Commons, {
+})
 
-Item.Warrior.Protection = {
-  PotionofPhantomFire              = Item(171349),
-}
+Item.Warrior.Arms = MergeTableByKey(Item.Warrior.Commons, {
+})
 
+Item.Warrior.Protection = MergeTableByKey(Item.Warrior.Commons, {
+})
