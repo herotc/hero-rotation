df98034c9ce749e8c7a1f2c70fead9c6f25da1dc
mhunk <47985590+mhunk@users.noreply.github.com>
2022-09-13 16:16:03 -0400

    Update symlink.bat
    Test Commit - Default install path

--- symlink.bat
+++ symlink.bat
@@ -1,6 +1,6 @@
 REM Modify the WoWRep var so it match you own setup. Make sure you have it surrounded by double quotes
 REM WoWRep  : World of warcraft main directory
-set WoWRep="G:\World of Warcraft"
+set WoWRep="C:\Program Files (x86)\World of Warcraft"
 set CWD=%cd%
 
 REM Don't touch anything bellow this if you aren't experienced
