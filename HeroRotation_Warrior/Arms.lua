93b7971b7faeb290687577e649e039b749e5ca66
Cilraaz <cilraaz@gmail.com>
2022-10-31 18:34:34 -0400

    fix(Arms): Fix init print message

--- HeroRotation_Warrior/Arms.lua
+++ HeroRotation_Warrior/Arms.lua
@@ -401,7 +401,7 @@ local function APL()
 end
 
 local function Init()
-  HR.Print("Arms Warrior rotation has not been updated for pre-patch 10.0. It may not function properly or may cause errors in-game.")
+  HR.Print("Arms Warrior rotation is currently a work in progress, but has been updated for patch 10.0.0.")
 end
 
 HR.SetAPL(71, APL, Init)
