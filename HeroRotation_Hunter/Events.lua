e4d62aa023e2310a3a850039b26648e7b806fc77
Cilraaz <cilraaz@gmail.com>
2023-09-11 17:17:59 -0400

    feat(Hunter): More pet tracking stuffs
    - Handle Play Dead and Wake Up, since for some god unknown reason Play Dead triggers UNIT_DIED!!!!!!

--- HeroRotation_Hunter/Events.lua
+++ HeroRotation_Hunter/Events.lua
@@ -24,16 +24,18 @@ Hunter.Pet = {}
 -- Pet Statuses are 0 (dismissed), 1 (alive), or 2 (dead)
 Hunter.Pet.Status = (Pet:Exists()) and 1 or 0
 Hunter.Pet.GUID = (Pet:Exists()) and Pet:GUID() or 0
+Hunter.Pet.FeignGUID = 0
 -- SummonSpells are Call Pet 1-5 and Revive Pet
 Hunter.Pet.SummonSpells = { 883, 83242, 83243, 83244, 83245, 982 }
 
 HL:RegisterForSelfCombatEvent(
   function(...)
-    local DestGUID, _, _, _, SpellID = select(8, ...)
+    local _, _, _, _, _, _, _, DestGUID, _, _, _, SpellID = ...
     for _, Spell in pairs(Hunter.Pet.SummonSpells) do
       if SpellID == Spell then
         Hunter.Pet.Status = 1
         Hunter.Pet.GUID = DestGUID
+        Hunter.Pet.FeignGUID = 0
       end
     end
   end
@@ -42,10 +44,11 @@ HL:RegisterForSelfCombatEvent(
 
 HL:RegisterForSelfCombatEvent(
   function(...)
-    local SpellID = select(12, ...)
+    local _, _, _, _, _, _, _, _, _, _, _, SpellID = ...
     if SpellID == 2641 then
       Hunter.Pet.Status = 0
       Hunter.Pet.GUID = 0
+      Hunter.Pet.FeignGUID = 0
     end
   end
   , "SPELL_CAST_SUCCESS"
@@ -53,11 +56,27 @@ HL:RegisterForSelfCombatEvent(
 
 HL:RegisterForCombatEvent(
   function(...)
-    local DestGUID = select(8, ...)
+    local _, _, _, _, _, _, _, DestGUID = ...
     if DestGUID == Hunter.Pet.GUID then
       Hunter.Pet.Status = 2
       Hunter.Pet.GUID = 0
     end
   end
   , "UNIT_DIED"
+)
+
+HL:RegisterForEvent(
+  function(...)
+    local _, CasterUnit, _, SpellID = ...
+    if CasterUnit ~= "player" then return end
+    if SpellID == 209997 then
+      Hunter.Pet.FeignGUID = Hunter.Pet.GUID
+    end
+    if SpellID == 210000 and Hunter.Pet.FeignGUID ~= 0 then
+      Hunter.Pet.GUID = Hunter.Pet.FeignGUID
+      Hunter.Pet.FeignGUID = 0
+      Hunter.Pet.Status = 1
+    end
+  end
+  , "UNIT_SPELLCAST_SUCCEEDED"
 )
