cce563b7f7d10e4113ffd8aa9cb26b2239e55a9f
Seny951 <seny951@hotmail.com>
2023-11-11 14:10:10 +0000

    outlaw APL updates

--- HeroRotation_Rogue/Rogue.lua
+++ HeroRotation_Rogue/Rogue.lua
@@ -170,6 +170,8 @@ Spell.Rogue.Outlaw = MergeTableByKey(Spell.Rogue.Commons, {
   AudacityBuff            = Spell(386270),
   BladeRush               = Spell(271877),
   CountTheOdds            = Spell(381982),
+  Crackshot				        = Spell(423703),
+  DeftManeuvers           = Spell(385835),
   Dreadblades             = Spell(343142),
   FanTheHammer            = Spell(381846),
   GhostlyStrike           = Spell(196937),
@@ -187,6 +189,7 @@ Spell.Rogue.Outlaw = MergeTableByKey(Spell.Rogue.Commons, {
   SummarilyDispatched     = Spell(381990),
   SwiftSlasher            = Spell(381988),
   TakeEmBySurpriseBuff    = Spell(385907),
+  UnderhandedUpperhand    = Spell(424044),
   Weaponmaster            = Spell(200733),
   -- Utility
   Gouge                   = Spell(1776),
@@ -264,6 +267,8 @@ Item.Rogue.Outlaw = {
   -- Trinkets
   ManicGrieftorch         = Item(194308, {13, 14}),
   WindscarWhetstone       = Item(137486, {13, 14}),
+  BeaconToTheBeyond       = Item(203963, {13, 14}),
+  DragonfireBombDispenser = Item(202610, {13, 14}),
 }
 
 Item.Rogue.Subtlety = {
