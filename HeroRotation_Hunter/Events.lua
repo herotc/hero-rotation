b27181a89ed2527570abcaabb9ae7dd3c9e56614
Cilraaz <cilraaz@gmail.com>
2023-12-01 00:55:32 -0500

    fix(Hunter): Tweak pet tracking
    - Use SPELLS_CHANGED instead of SPELL_SUMMON for determining when a pet comes into existence
    - Delay zeroing out pet data when casting Dismiss Pet, as SPELL_CAST_SUCCESS fires before SPELLS_CHANGED, causing the SPELLS_CHANGED registration to set status back to 1.
    - Fixes Issue #1021

--- HeroRotation_Hunter/Events.lua
+++ HeroRotation_Hunter/Events.lua
@@ -15,40 +15,39 @@ local Pet    = Unit.Pet
 local Spell  = HL.Spell
 local Item   = HL.Item
 -- Lua
+local C_TimerAfter = C_Timer.After
 -- File Locals
 HR.Commons.Hunter = {}
 local Hunter = HR.Commons.Hunter
 
 Hunter.Pet = {}
 
--- Pet Statuses are 0 (dismissed), 1 (alive), or 2 (dead)
+-- Pet Statuses are 0 (dismissed), 1 (alive), or 2 (dead/feigned)
 Hunter.Pet.Status = (Pet:Exists()) and 1 or 0
 Hunter.Pet.GUID = (Pet:Exists()) and Pet:GUID() or 0
 Hunter.Pet.FeignGUID = 0
--- SummonSpells are Call Pet 1-5 and Revive Pet
-Hunter.Pet.SummonSpells = { 883, 83242, 83243, 83244, 83245, 982 }
 
-HL:RegisterForSelfCombatEvent(
-  function(...)
-    local _, _, _, _, _, _, _, DestGUID, _, _, _, SpellID = ...
-    for _, Spell in pairs(Hunter.Pet.SummonSpells) do
-      if SpellID == Spell then
-        Hunter.Pet.Status = 1
-        Hunter.Pet.GUID = DestGUID
-        Hunter.Pet.FeignGUID = 0
-      end
+HL:RegisterForEvent(
+  function()
+    if Hunter.Pet.Status == 0 and Pet:Exists() then
+      Hunter.Pet.Status = 1
+      Hunter.Pet.GUID = Pet:GUID()
+      Hunter.Pet.FeignGUID = 0
     end
   end
-  , "SPELL_SUMMON"
+  , "SPELLS_CHANGED"
 )
 
 HL:RegisterForSelfCombatEvent(
   function(...)
     local _, _, _, _, _, _, _, _, _, _, _, SpellID = ...
     if SpellID == 2641 then
-      Hunter.Pet.Status = 0
-      Hunter.Pet.GUID = 0
-      Hunter.Pet.FeignGUID = 0
+      -- Delay for 1s, as SPELL_CAST_SUCCESS fires before SPELLS_CHANGED when casting Dismiss Pet.
+      C_TimerAfter(1, function()
+        Hunter.Pet.Status = 0
+        Hunter.Pet.GUID = 0
+        Hunter.Pet.FeignGUID = 0
+      end)
     end
   end
   , "SPELL_CAST_SUCCESS"
