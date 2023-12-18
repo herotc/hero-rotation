a90e51e96735f5ad033eb728095d312635820ca8
Cilraaz <cilraaz@gmail.com>
2023-12-18 11:55:42 -0500

    fix(Feral): Fix range out of bounds error

--- HeroRotation_Druid/Feral.lua
+++ HeroRotation_Druid/Feral.lua
@@ -665,7 +665,7 @@ local function Cooldown()
       if Cast(I.MydasTalisman, nil, Settings.Commons.DisplayStyle.Trinkets) then return "mydas_talisman cooldown 30"; end
     end
     if I.BandolierofTwistedBlades:IsEquippedAndReady() then
-      if Cast(I.BandolierofTwistedBlades, nil, Settings.Commons.DisplayStyle.Trinkets, not Target:IsInMeleeRange(6)) then return "bandolier_of_twisted_blades cooldown 32"; end
+      if Cast(I.BandolierofTwistedBlades, nil, Settings.Commons.DisplayStyle.Trinkets, not Target:IsInMeleeRange(5)) then return "bandolier_of_twisted_blades cooldown 32"; end
     end
     if I.FyrakksTaintedRageheart:IsEquippedAndReady() then
       if Cast(I.FyrakksTaintedRageheart, nil, Settings.Commons.DisplayStyle.Trinkets, not Target:IsInMeleeRange(10)) then return "fyrakks_tainted_rageheart cooldown 34"; end
