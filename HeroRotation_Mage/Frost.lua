8193c04ff086222158ff5a37f5230af30d1346d1
Cilraaz <cilraaz@gmail.com>
2024-02-02 21:03:37 -0500

    feat(FrostMage): Remove GCDRemains check for Belor'relos

--- HeroRotation_Mage/Frost.lua
+++ HeroRotation_Mage/Frost.lua
@@ -158,7 +158,7 @@ local function Cooldowns()
   end
   if Settings.Commons.Enabled.Trinkets then
     -- use_item,name=belorrelos_the_suncaller,use_off_gcd=1,if=(gcd.remains>gcd.max-0.1|fight_remains<5)&time>5
-    if I.BelorrelostheSuncaller:IsEquippedAndReady() and ((Player:GCDRemains() > Player:GCD() - 0.1 or FightRemains < 5) and HL.CombatTime() > 5) then
+    if I.BelorrelostheSuncaller:IsEquippedAndReady() and (HL.CombatTime() > 5) then
       if Cast(I.BelorrelostheSuncaller, nil, Settings.Commons.DisplayStyle.Trinkets, not Target:IsInRange(10)) then return "belorrelos_the_suncaller cd 10"; end
     end
     -- use_item,name=balefire_branch,if=(!talent.ray_of_frost&active_enemies<=2&buff.icy_veins.up&prev_gcd.1.glacial_spike|remaining_winters_chill=1&cooldown.ray_of_frost.up&time>1&active_enemies<=2|cooldown.cone_of_cold.up&prev_gcd.1.comet_storm&active_enemies>=3)|fight_remains<20
