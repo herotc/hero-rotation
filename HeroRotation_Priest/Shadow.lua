66e8018e1b2875857cf03671efdcb565faf8994c
Cilraaz <cilraaz@gmail.com>
2024-02-22 19:07:21 -0500

    Revert "feat(Shadow): Remove custom opener"
    This reverts commit 28651eabe4bc174d2c722405207cd957b3c08b2c.

--- HeroRotation_Priest/Shadow.lua
+++ HeroRotation_Priest/Shadow.lua
@@ -76,6 +76,8 @@ local VarPreferVT = false
 local Fiend = (S.Mindbender:IsAvailable()) and S.Mindbender or S.Shadowfiend
 local VarFiendUp = false
 local VarFiendRemains = 0
+local VarOpened = false
+local VarFirstTar = nil
 local Flay
 local GCDMax
 
@@ -95,6 +97,8 @@ HL:RegisterForEvent(function()
   VarPreferVT = false
   VarFiendUp = false
   VarFiendRemains = 0
+  VarOpened = false
+  VarFirstTar = nil
 end, "PLAYER_REGEN_ENABLED")
 
 HL:RegisterForEvent(function()
@@ -243,6 +247,7 @@ local function EvaluateCycleVTAoE2(TargetUnit)
 end
 
 local function Precombat()
+  VarFirstTar = Target:GUID()
   -- flask
   -- food
   -- augmentation
@@ -356,6 +361,69 @@ local function Trinkets()
   -- use_item,name=desperate_invokers_codex,if=equipped.desperate_invokers_codex
   if Settings.Commons.Enabled.Trinkets and I.DesperateInvokersCodex:IsEquippedAndReady() then
     if Cast(I.DesperateInvokersCodex, nil, Settings.Commons.DisplayStyle.Trinkets) then return "desperate_invokers_codex trinkets 24"; end
+  end
+end
+
+local function Opener()
+  -- shadow_crash,if=!debuff.vampiric_touch.up
+  if S.ShadowCrash:IsCastable() and not VarPreferVT and (Target:DebuffDown(S.VampiricTouchDebuff)) then
+    if Cast(S.ShadowCrash, Settings.Shadow.GCDasOffGCD.ShadowCrash, nil, not Target:IsInRange(40)) then return "shadow_crash opener 2"; end
+  end
+  -- vampiric_touch,if=!debuff.vampiric_touch.up&(!cooldown.shadow_crash.ready|!talent.shadow_crash)
+  if S.VampiricTouch:IsCastable() and (Target:DebuffDown(S.VampiricTouchDebuff) and (S.ShadowCrash:CooldownDown() or not S.ShadowCrash:IsAvailable() or S.ShadowCrash:InFlight())) then
+    if Cast(S.VampiricTouch, nil, nil, not Target:IsSpellInRange(S.VampiricTouch)) then return "vampiric_touch opener 3"; end
+  end
+  -- mindbender
+  if Fiend:IsCastable() then
+    if Cast(Fiend, Settings.Shadow.GCDasOffGCD.Mindbender) then return "mindbender opener 4"; end
+  end
+  -- dark_ascension,if=talent.dark_ascension
+  if S.DarkAscension:IsCastable() then
+    if Cast(S.DarkAscension, Settings.Shadow.GCDasOffGCD.DarkAscension) then return "dark_ascension opener 6"; end
+  end
+  if S.VoidEruption:IsAvailable() then
+    -- shadow_word_death,if=talent.inescapable_torment&talent.void_eruption&prev_gcd.mind_blast
+    -- Note: 20+ seconds since last cast, since we only want one cast during the opener.
+    if S.ShadowWordDeath:IsCastable() and S.InescapableTorment:IsAvailable() and Player:PrevGCDP(1, S.MindBlast) and S.ShadowWordDeath:TimeSinceLastCast() > 20 then
+      if Cast(S.ShadowWordDeath, Settings.Shadow.GCDasOffGCD.ShadowWordDeath, nil, not Target:IsSpellInRange(S.ShadowWordDeath)) then return "shadow_word_death opener 8"; end
+    end
+    -- mind_blast,if=talent.void_eruption
+    if S.MindBlast:IsCastable() then
+      if Cast(S.MindBlast, nil, nil, not Target:IsSpellInRange(S.MindBlast)) then return "mind_blast opener 10"; end
+    end
+    -- void_eruption,if=talent.void_eruption
+    if S.VoidEruption:IsCastable() then
+      if Cast(S.VoidEruption, Settings.Shadow.GCDasOffGCD.VoidEruption) then return "void_eruption opener 12"; end
+    end
+  end
+  -- power_infusion,if=buff.voidform.up|buff.dark_ascension.up
+  if CDsON() and S.PowerInfusion:IsCastable() and Settings.Shadow.SelfPI and (Player:BuffUp(S.VoidformBuff) or Player:BuffUp(S.DarkAscension)) then
+    if Cast(S.PowerInfusion, Settings.Shadow.OffGCDasOffGCD.PowerInfusion) then return "power_infusion opener 14"; end
+  end
+  -- use_items
+  if Settings.Commons.Enabled.Trinkets or Settings.Commons.Enabled.Items then
+    local ShouldReturn = Trinkets(); if ShouldReturn then return ShouldReturn; end
+  end
+  -- void_bolt,if=talent.void_eruption
+  if S.VoidBolt:IsCastable() then
+    if Cast(S.VoidBolt, nil, nil, not Target:IsInRange(40)) then return "void_bolt opener 16"; end
+  end
+  -- devouring_plague
+  if S.DevouringPlague:IsReady() then
+    if Cast(S.DevouringPlague, nil, nil, not Target:IsSpellInRange(S.DevouringPlague)) then return "devouring_plague opener 18"; end
+  end
+  -- Note: Just in case there is not enough Insanity for devouring_plague...
+  -- mind_blast
+  if S.MindBlast:IsCastable() then
+    if Cast(S.MindBlast, nil, nil, not Target:IsSpellInRange(S.MindBlast)) then return "mind_blast opener 20"; end
+  end
+  -- mind_spike
+  if S.MindSpike:IsCastable() then
+    if Cast(S.MindSpike, nil, nil, not Target:IsSpellInRange(S.MindSpike)) then return "mind_spike opener 22"; end
+  end
+  -- mind_flay
+  if S.MindFlay:IsCastable() then
+    if Cast(S.MindFlay, nil, nil, not Target:IsSpellInRange(S.MindFlay)) then return "mind_flay opener 24"; end
   end
 end
 
@@ -688,6 +756,14 @@ local function APL()
     VarPreferVT = Settings.Shadow.PreferVTWhenSTinDungeon and EnemiesCount10ySplash == 1 and Player:IsInDungeonArea() and Player:IsInParty() and not Player:IsInRaidArea()
     -- variable,name=pool_for_cds,op=set,value=(cooldown.void_eruption.remains<=gcd.max*3&talent.void_eruption|cooldown.dark_ascension.up&talent.dark_ascension)|talent.void_torrent&talent.psychic_link&cooldown.void_torrent.remains<=4&(!raid_event.adds.exists&spell_targets.vampiric_touch>1|raid_event.adds.in<=5|raid_event.adds.remains>=6&!variable.holding_crash)&!buff.voidform.up
     VarPoolForCDs = ((S.VoidEruption:CooldownRemains() <= Player:GCD() * 3 and S.VoidEruption:IsAvailable() or S.DarkAscension:CooldownUp() and S.DarkAscension:IsAvailable()) or S.VoidTorrent:IsAvailable() and S.PsychicLink:IsAvailable() and S.VoidTorrent:CooldownRemains() <= 4 and Player:BuffDown(S.VoidformBuff))
+    -- Manually added: Attempt at an opener
+    if VarFirstTar == nil then VarFirstTar = Target:GUID() end
+    if VarOpened == false and Settings.Shadow.UseOpener and Target:GUID() == VarFirstTar and not DotsUp(Target, true) then
+      local ShouldReturn = Opener(); if ShouldReturn then return ShouldReturn; end
+      if HR.CastAnnotated(S.Pool, false, "WAIT") then return "Pool for Opener()"; end
+    else
+      VarOpened = true
+    end
     -- run_action_list,name=aoe,if=active_enemies>2
     if EnemiesCount10ySplash > 2 then
       local ShouldReturn = AoE(); if ShouldReturn then return ShouldReturn; end
