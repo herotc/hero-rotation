88f7719db40616aa1091f15aabd873f897173a49
James Sen <seny951@hotmail.com>
2024-08-17 00:10:54 +0100

    feat(Outlaw/Subtlety): Update to latest APL
    * outlaw and sub apl updates
    
    * added a couple of missing spell ids

--- HeroRotation_Rogue/Outlaw.lua
+++ HeroRotation_Rogue/Outlaw.lua
@@ -760,13 +760,21 @@ local function APL ()
     -- Opener
     if Everyone.TargetIsValid() then
       -- Precombat CDs
+      if I.ImperfectAscendancySerum:IsEquippedAndReady() then
+          if Cast(I.ImperfectAscendancySerum, nil, Settings.CommonsDS.DisplayStyle.Trinkets, not Target:IsItemInRange(I.ImperfectAscendancySerum)) then return "Imperfect Ascendancy Serum"; end
+      end
+      -- actions.precombat+=/adrenaline_rush,precombat_seconds=2,if=talent.improved_adrenaline_rush&talent.keep_it_rolling&talent.loaded_dice
+      if S.AdrenalineRush:IsReady() and S.ImprovedAdrenalineRush:IsAvailable() and S.KeepItRolling:IsAvailable()
+        and S.LoadedDice:IsAvailable() then
+          if Cast(S.AdrenalineRush) then return "Cast Adrenaline Rush (Opener KiR)" end
+      end
       -- actions.precombat+=/roll_the_bones,precombat_seconds=2
       -- Use same extended logic as a normal rotation for between pulls
       if S.RolltheBones:IsReady() and not Player:DebuffUp(S.Dreadblades) and (RtB_Buffs() == 0 or RtB_Reroll()) then
         if Cast(S.RolltheBones, Settings.Outlaw.GCDasOffGCD.RollTheBones) then return "Cast Roll the Bones (Opener)" end
       end
       -- actions.precombat+=/adrenaline_rush,precombat_seconds=3,if=talent.improved_adrenaline_rush
-      if S.AdrenalineRush:IsReady() and S.ImprovedAdrenalineRush:IsAvailable() and ComboPoints <= 2 then
+      if S.AdrenalineRush:IsReady() and S.ImprovedAdrenalineRush:IsAvailable() then
         if Cast(S.AdrenalineRush) then return "Cast Adrenaline Rush (Opener)" end
       end
       -- actions.precombat+=/slice_and_dice,precombat_seconds=1
--- HeroRotation_Rogue/Rogue.lua
+++ HeroRotation_Rogue/Rogue.lua
@@ -73,6 +73,7 @@ Spell.Rogue.Commons = {
   -- Talents
   AcrobaticStrikes        = Spell(455143),
   Alacrity                = Spell(193539),
+  ClearTheWitnessesBuff   = Spell(457179),
   ColdBlood               = Spell(382245),
   CoupDeGrace             = Spell(441776),
   DarkestNight            = Spell(457058),
@@ -86,15 +87,18 @@ Spell.Rogue.Commons = {
   EchoingReprimand3       = Spell(323559),
   EchoingReprimand4       = Spell(323560),
   EchoingReprimand5       = Spell(354838),
+  EscalatingBlade         = Spell(441786),
   FateboundCoin           = Spell(453012),
   FateboundCoinHeads      = Spell(456479),
   FateboundCoinTails      = Spell(452538),
   FateboundLuckyCoin      = Spell(461818),
   FatefulEnding           = Spell(454428),
+  Fazed                   = Spell(460927),
   FindWeakness            = Spell(91023),
   FindWeaknessDebuff      = Spell(316220),
   FlawlessForm            = Spell(441321),
   FlawlessFormBuff        = Spell(441326),
+  FollowTheBlood          = Spell(457068),
   HandOfFate              = Spell(452536),
   ImprovedAmbush          = Spell(381620),
   MarkedforDeath          = Spell(137619),
@@ -282,6 +286,7 @@ Spell.Rogue.Subtlety = MergeTableByKey(Spell.Rogue.Commons, {
   PreyontheWeakDebuff     = Spell(255909),
   Premeditation           = Spell(343160),
   PremeditationBuff       = Spell(343173),
+  ReplicatingShadows      = Spell(382506),
   SecretStratagem         = Spell(394320),
   SecretTechnique         = Spell(280719),
   Shadowcraft             = Spell(426594),
@@ -327,6 +332,7 @@ Item.Rogue.Subtlety = {
   Mirror                  = Item(207581, {13, 14}),
   IrideusFragment         = Item(193743, {13, 14}),
   ImperfectAscendancySerum = Item(225654, {13, 14}),
+  TreacherousTransmitter  = Item(221023, {13, 14})
 }
 
 function Commons.StealthSpell()
