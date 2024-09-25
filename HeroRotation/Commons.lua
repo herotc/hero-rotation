e27f93fb2e0ca74a8355b5f3fc8abf14f9325604
Cilraaz <cilraaz@gmail.com>
2024-09-25 12:34:58 -0400

    feat(Commons): Simplify Brann check in GroupBuffMissing

--- HeroRotation/Commons.lua
+++ HeroRotation/Commons.lua
@@ -167,12 +167,8 @@ function Commons.GroupBuffMissing(spell)
   local TotalChars = 0
   local BuffedChars = 0
   for _, Char in pairs(Group) do
-    if Char:Exists() and not Char:IsDeadOrGhost() and Char:IsInRange(range) then
+    if Char:Exists() and not Char:IsDeadOrGhost() and Char:IsInRange(range) and not (Player:IsInDelve() and Char:Name() == "Brann Bronzebeard") then
       TotalChars = TotalChars + 1
-      if Player:IsInDelve() and Char:Name() == "Brann Bronzebeard" then
-        -- If in a Delve with Brann, remove him from the total count.
-        TotalChars = TotalChars - 1
-      end
       if spell:ID() == 381748 then -- Blessing of the Bronze
         local _, _, CharClass = Char:Class()
         if Char:BuffUp(Spell(BotBBuffIDs[CharClass]), true) then
