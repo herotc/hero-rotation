8a47eb7496c9cbfbeee65cb746cce9821e2c9497
Tom Herbert <18316812+taherbert@users.noreply.github.com>
2024-10-05 20:51:53 -0700

    pass OnUseExcludes to GetTrinketData

--- HeroRotation_DemonHunter/Vengeance.lua
+++ HeroRotation_DemonHunter/Vengeance.lua
@@ -52,7 +52,7 @@ local VarTrinket1BL, VarTrinket2BL
 local VarTrinket1Buffs, VarTrinket2Buffs
 local VarTrinketFailures = 0
 local function SetTrinketVariables()
-  local T1, T2 = Player:GetTrinketData()
+  local T1, T2 = Player:GetTrinketData(OnUseExcludes)
 
   -- If we don't have trinket items, try again in 5 seconds.
   if VarTrinketFailures < 5 and ((T1.ID == 0 or T2.ID == 0) or (T1.Level == 0 or T2.Level == 0) or (T1.SpellID > 0 and not T1.Usable or T2.SpellID > 0 and not T2.Usable)) then
