7b30b6a5fd73736d2c124a893d7209a6e0c11368
Cilraaz <cilraaz@gmail.com>
2025-01-28 11:12:05 -0500

    feat(Arms): TWW S2 4pc buff name change

--- HeroRotation_Warrior/Warrior.lua
+++ HeroRotation_Warrior/Warrior.lua
@@ -141,11 +141,11 @@ Spell.Warrior.Arms = MergeTableByKey(Spell.Warrior.Commons, {
   Warbreaker                            = Spell(262161),
   -- Buffs
   CollateralDamageBuff                  = Spell(334783),
-  HedgedBetsBuff                        = Spell(1216556), -- TWW S2 4pc
   JuggernautBuff                        = Spell(383290),
   LethalBlowsBuff                       = Spell(455485), -- TWW S1 4pc
   MartialProwessBuff                    = Spell(7384),
   MercilessBonegrinderBuff              = Spell(383316),
+  PayThemBackBuff                       = Spell(1216556), -- TWW S2 4pc
   StrikeVulnerabilitiesBuff             = Spell(394173),
   SuddenDeathBuff                       = Spell(52437),
   SweepingStrikesBuff                   = Spell(260708),
