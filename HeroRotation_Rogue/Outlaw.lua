f5bc20f1301d08fb3769faf77dc245c9f757dcb1
Cilraaz <cilraaz@gmail.com>
2025-03-14 23:24:55 -0400

    fix(Outlaw): Use return value from GetUseableItems instead of re-calculating range

--- HeroRotation_Rogue/Outlaw.lua
+++ HeroRotation_Rogue/Outlaw.lua
@@ -900,13 +900,7 @@ local function CDs ()
     -- # Default conditions for usable items.
     -- actions.cds+=/use_items,slots=trinket1,if=debuff.between_the_eyes.up|trinket.1.has_stat.any_dps|fight_remains<=20
     -- actions.cds+=/use_items,slots=trinket2,if=debuff.between_the_eyes.up|trinket.2.has_stat.any_dps|fight_remains<=20
-    local TrinketToUse = Player:GetUseableItems(OnUseExcludes, 13) or Player:GetUseableItems(OnUseExcludes, 14)
-    local TrinketSpell
-    local TrinketRange = 100
-    if TrinketToUse then
-      TrinketSpell = TrinketToUse:OnUseSpell()
-      TrinketRange = (TrinketSpell and TrinketSpell.MaximumRange > 0 and TrinketSpell.MaximumRange <= 100) and TrinketSpell.MaximumRange or 100
-    end
+    local TrinketToUse, _, TrinketRange = Player:GetUseableItems(OnUseExcludes, 13) or Player:GetUseableItems(OnUseExcludes, 14)
     if TrinketToUse and (Player:BuffUp(S.BetweentheEyes) or HL.BossFilteredFightRemains("<", 20) or TrinketToUse:HasStatAnyDps()) then
       if Cast(TrinketToUse, nil, Settings.CommonsDS.DisplayStyle.Trinkets, not Target:IsInRange(TrinketRange)) then
         return "Generic use_items for " .. TrinketToUse:Name()
