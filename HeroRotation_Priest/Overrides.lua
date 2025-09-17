9c9a5a3f32baa704f8d6368f0d0ae40d6bda61df
J <37181202+joshjau@users.noreply.github.com>
2025-09-17 10:30:25 +0800

    Void Volley Insanity prediction added

--- HeroRotation_Priest/Overrides.lua
+++ HeroRotation_Priest/Overrides.lua
@@ -40,6 +40,8 @@ HL.AddCoreOverride ("Player.Insanity",
         return Insanity + 30
       elseif Player:IsCasting(SpellShadow.VoidTorrent) then
         return Insanity + (24 / SpellShadow.VoidTorrent:BaseDuration()) * Player:CastRemains()
+      elseif Player:IsCasting(SpellShadow.VoidVolley) then
+        return Insanity + 10
       else
         return Insanity
       end
